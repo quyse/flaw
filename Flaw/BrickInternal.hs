afd587ce9a3543ea496405499d74cbdc1ce41b66
Quyse Lert <quyse0@gmail.com>
2014-10-08 22:54:56 +0000

    small refactor

--- Flaw/BrickInternal.hs
+++ Flaw/BrickInternal.hs
@@ -69,14 +69,14 @@ genPrimBrickable :: Name -> Q [Dec]
 genPrimBrickable name = do
 	let originalType = ConT name
 	-- declaration of Brick data instance
-	brickConName <- newName $ nameBase name ++ "_Brick"
-	let brickDataDec = DataInstD [] ''Brick [originalType] [NormalC brickConName [(NotStrict, originalType)]] []
+	bName <- newName $ nameBase name ++ "_Brick"
+	let brickDataDec = DataInstD [] ''Brick [originalType] [NormalC bName [(NotStrict, originalType)]] []
 	-- declaration of brick method
 	brickDec <- do
 		-- method param
 		paramName <- newName "a"
 		let hashE = AppE (VarE 'hash) $ AppE (VarE 'encode) $ VarE paramName
-		let body = NormalB $ AppE (AppE hashedBrickE hashE) $ AppE (ConE brickConName) $ VarE paramName
+		let body = NormalB $ AppE (AppE hashedBrickE hashE) $ AppE (ConE bName) $ VarE paramName
 		let clause = Clause [VarP paramName] body []
 		return $ FunD 'brick [clause]
 	-- unite all declarations
@@ -84,81 +84,94 @@ genPrimBrickable name = do
 	-- return instance
 	return [InstanceD [] (AppT (ConT ''Brickable) originalType) decs]
 
+-- | Brick constructor.
+data BrickCon = BrickCon
+	{ brickConName :: Name
+	, brickConOriginalPat :: Pat
+	, brickConBrickExp :: Exp
+	, brickConBrickCon :: Con
+	}
+
+-- | Helper to create type HashedBrick t.
+hbType :: Type -> Type
+hbType t = AppT (ConT ''HashedBrick) t
+-- | Helper to propagate HashedBrick into StrictType.
+hbStrictType :: (Strict, Type) -> (Strict, Type)
+hbStrictType (s, t) = (s, hbType t)
+-- | Helper to propagate HashedBrick into VarStrictType.
+hbVarStrictType :: (Name, Strict, Type) -> (Name, Strict, Type)
+hbVarStrictType (n, s, t) = (n, s, hbType t)
+
+-- | Get name of TyVarBndr
+getTvbName :: TyVarBndr -> Name
+getTvbName tvb = case tvb of
+	PlainTV name -> name
+	KindedTV name _ -> name
+
+-- | Get name and temp param name for VarStrictType
+tempVst :: (Name, Strict, Type) -> Q (Name, Name)
+tempVst (fieldName, _, _) = do
+	paramName <- newName $ nameBase fieldName
+	return (fieldName, paramName)
+
+-- | Generate new name for bricky things.
+brickName :: Name -> Q Name
+brickName name = newName $ nameBase name ++ "_Brick"
+
+-- | Transform original constructor into brick constructor.
+brickCon :: Con -> Q BrickCon
+brickCon con = do
+	case con of
+		NormalC name sts -> do
+			bName <- brickName name
+			params <- mapM (\_ -> newName "p") sts
+			return BrickCon
+				{ brickConName = bName
+				, brickConOriginalPat = ConP name $ map VarP params
+				, brickConBrickExp = foldl AppE (ConE bName) $ map ((AppE brickE) . VarE) params
+				, brickConBrickCon = NormalC bName $ map hbStrictType sts
+				}
+		RecC name vsts -> do
+			bName <- brickName name
+			params <- mapM tempVst vsts
+			return BrickCon
+				{ brickConName = bName
+				, brickConOriginalPat = RecP name [(fieldName, VarP paramName) | (fieldName, paramName) <- params]
+				, brickConBrickExp = RecConE name [(fieldName, AppE brickE $ VarE paramName) | (fieldName, paramName) <- params]
+				, brickConBrickCon = RecC bName $ map hbVarStrictType vsts
+				}
+		InfixC st1 name st2 -> do
+			bName <- brickName name
+			p1 <- newName "p1"
+			p2 <- newName "p2"
+			return BrickCon
+				{ brickConName = bName
+				, brickConOriginalPat = InfixP (VarP p1) name (VarP p2)
+				, brickConBrickExp = InfixE (Just $ AppE brickE $ VarE p1) (VarE bName) (Just $ AppE brickE $ VarE p2)
+				, brickConBrickCon = InfixC (hbStrictType st1) bName (hbStrictType st2)
+				}
+		ForallC bndrs cxt c -> do
+			bc <- brickCon c
+			return bc
+				{ brickConBrickCon = ForallC bndrs cxt (brickConBrickCon bc)
+				}
+
 -- | Generate Brickable instance for a given type.
 genBrickable :: Name -> Q [Dec]
 genBrickable name = reify name >>= genBrickableFromInfo where
-	-- | Helper to create type HashedBrick t.
-	hbType t = AppT (ConT ''HashedBrick) t
-	-- | Helper to propagate HashedBrick into StrictType.
-	hbStrictType (s, t) = (s, hbType t)
-	-- | Helper to propagate HashedBrick into VarStrictType.
-	hbVarStrictType (n, s, t) = (n, s, hbType t)
-	-- | Get name of TyVarBndr
-	getTvbName tvb = case tvb of
-		PlainTV name -> name
-		KindedTV name _ -> name
-	-- | Generate new name for a constructor.
-	newConName name = newName $ nameBase name ++ "_Brick"
-	-- | Get name and temp param name for VarStrictType
-	tempVst (fieldName, _, _) = do
-		paramName <- newName $ nameBase fieldName
-		return (fieldName, paramName)
-	-- | Transform original constructor into brick constructor.
-	-- Returns name, pattern for original constructor,
-	-- expression for initializing brick,
-	-- and Brick constructor.
-	transformCon :: Con -> Q (Name, Pat, Exp, Con)
-	transformCon con = do
-		case con of
-			NormalC name sts -> do
-				bName <- newConName name
-				params <- mapM (\_ -> newName "p") sts
-				return
-					( bName
-					, ConP name $ map VarP params
-					, foldl AppE (ConE bName) $ map ((AppE brickE) . VarE) params
-					, NormalC bName $ map hbStrictType sts
-					)
-			RecC name vsts -> do
-				bName <- newConName name
-				params <- mapM tempVst vsts
-				return
-					( bName
-					, RecP name [(fieldName, VarP paramName) | (fieldName, paramName) <- params]
-					, RecConE name [(fieldName, AppE brickE $ VarE paramName) | (fieldName, paramName) <- params]
-					, RecC bName $ map hbVarStrictType vsts
-					)
-			InfixC st1 name st2 -> do
-				bName <- newConName name
-				p1 <- newName "p1"
-				p2 <- newName "p2"
-				return
-					( bName
-					, InfixP (VarP p1) name (VarP p2)
-					, InfixE (Just $ AppE brickE $ VarE p1) (VarE bName) (Just $ AppE brickE $ VarE p2)
-					, InfixC (hbStrictType st1) bName (hbStrictType st2)
-					)
-			ForallC bndrs cxt c -> do
-				(tn, tp, te, tc) <- transformCon c
-				return
-					( tn
-					, tp
-					, te
-					, ForallC bndrs cxt tc
-					)
 	-- | Generate Brickable instance for given type info.
 	genBrickableFromInfo (TyConI (DataD cxt name tvbs cons derivings)) = do
 		-- construct original type
 		let originalType = foldl AppT (ConT name) $ map (VarT . getTvbName) tvbs
 		-- transform constructors
-		brickCons <- mapM transformCon cons
+		brickCons <- mapM brickCon cons
 		-- declaration of Brick data instance
-		let brickDataDec = DataInstD [] ''Brick [originalType] [c | (n, p, e, c) <- brickCons] []
+		let brickDataDec = DataInstD [] ''Brick [originalType] (map brickConBrickCon brickCons) []
 		-- declaration of brick method
 		brickDec <- do
 			-- method param
 			paramName <- newName "a"
-			let matches = [Match p (NormalB $ AppE (AppE hashedBrickE emptyHashE) e) [] | (n, p, e, c) <- brickCons]
+			let matches = [Match (brickConOriginalPat bc) (NormalB $ AppE (AppE hashedBrickE emptyHashE) $ brickConBrickExp bc) [] | bc <- brickCons]
 			let clause = Clause [VarP paramName] (NormalB $ CaseE (VarE paramName) matches) []
 			return $ FunD 'brick [clause]
 		-- unite all declarations
