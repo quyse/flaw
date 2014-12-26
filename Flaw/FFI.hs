129190940044e11376d2cad68dbb21efff438539
Quyse Lert <quyse0@gmail.com>
2014-12-26 17:08:59 +0000

    add generation of DX11-style desc structs with unions

--- Flaw/FFI.hs
+++ Flaw/FFI.hs
@@ -12,6 +12,7 @@ module Flaw.FFI
 	, unwrapEnum
 	, genEnum
 	, genStruct
+	, genStructWithEndUnion
 	) where
 
 import Control.Monad
@@ -131,28 +132,32 @@ processField typeName ft fn fc prevEndExp = do
 		, fieldDecs = decs
 		}
 
-processFields :: String -> [(TypeQ, String, Int)] -> Q [Field]
+-- | Create ''Field objects for fields.
+-- Calculates offsets and other stuff, starting from zero offset, and chaining end offsets.
+processFields :: String -> [(TypeQ, String, Int)] -> Q ([Field], ExpQ)
 processFields typeName fs = pf [| 0 |] fs where
 	pf prevEndExp ((ft, fn, fc) : nfs) = do
 		f <- processField typeName ft fn fc prevEndExp
-		nextFields <- pf (fieldEnd f) nfs
-		return $ f : nextFields
-	pf _ [] = return []
+		(nextFields, nextEndExp) <- pf (fieldEnd f) nfs
+		return (f : nextFields, nextEndExp)
+	pf prevEndExp [] = return ([], prevEndExp)
+
+dataFieldDec :: Field -> Q (Name, Strict, Type)
+dataFieldDec field = do
+	ft <- fieldType field
+	return (fieldName field, NotStrict, ft)
 
 -- | Generate struct data type.
 genStruct :: String -> [(TypeQ, String, Int)] -> Q [Dec]
 genStruct typeName fs = do
-	fields <- processFields typeName fs
-	let dataFieldDec field = do
-		ft <- fieldType field
-		return (fieldName field, NotStrict, ft)
+	(fields, endExp) <- processFields typeName fs
 	dataDec <- dataD (return []) (mkName typeName) [] [recC (mkName typeName) $ map dataFieldDec fields] [''Show]
 	fieldsDecs <- liftM concat $ sequence $ map (sequence . fieldDecs) fields
 	let sizeOfName = mkName $ "struct_sizeOf_" ++ typeName
 	let alignmentName = mkName $ "struct_alignment_" ++ typeName
 	structDecs <- sequence
 		[ sigD sizeOfName [t| Int |]
-		, valD (varP sizeOfName) (normalB $ foldl1 (\a b -> [| $(a) + $(b) |]) $ map fieldSizeOf fields) []
+		, valD (varP sizeOfName) (normalB endExp) []
 		, sigD alignmentName [t| Int |]
 		, valD (varP alignmentName) (normalB $ foldl1 (\a b -> [| lcm $(a) $(b) |]) $ map fieldAlignment fields) []
 		]
@@ -171,3 +176,51 @@ genStruct typeName fs = do
 			poke addr value = $(doE [noBindS $ appE (appE (fieldPoke field) [| addr |]) $ appE (varE $ fieldName field) [| value |] | field <- fields])
 		|]
 	return $ dataDec : fieldsDecs ++ structDecs ++ instanceDecs
+
+-- | Generate D3D11-style struct with union of types in the end, selected by one of the fields.
+-- Generates multiple constructors selected by selector.
+genStructWithEndUnion :: String -> [(TypeQ, String, Int)] -> Int -> [(String, TypeQ, String)] -> Q [Dec]
+genStructWithEndUnion typeName hfs selectorIndex ufs = do
+	(headerFields, headerEndExp) <- processFields typeName hfs
+	unionFields <- mapM (\(_ufe, uft, ufn) -> processField typeName uft ufn 0 headerEndExp) ufs
+	dataDec <- dataD (return []) (mkName typeName) [] [recC (mkName $ typeName ++ "_" ++ (fieldNameStr unionField)) $ map dataFieldDec $ headerFields ++ [unionField] | unionField <- unionFields] [''Show]
+	headerFieldsDecs <- liftM concat $ sequence $ map (sequence . fieldDecs) headerFields
+	unionFieldsDecs <- liftM concat $ sequence $ map (sequence . fieldDecs) unionFields
+	let sizeOfName = mkName $ "struct_sizeOf_" ++ typeName
+	let alignmentName = mkName $ "struct_alignment_" ++ typeName
+	structDecs <- sequence
+		[ sigD sizeOfName [t| Int |]
+		, valD (varP sizeOfName) (normalB $ [| maximum $(listE $ map fieldEnd unionFields) |]) []
+		, sigD alignmentName [t| Int |]
+		, valD (varP alignmentName) (normalB $ foldl1 (\a b -> [| lcm $(a) $(b) |]) $ map fieldAlignment (headerFields ++ unionFields)) []
+		]
+	let peekFieldDec addr field = do
+		varName <- newName $ "tmp_" ++ fieldNameStr field
+		return (varName, return (fieldName field, VarE varName), bindS (varP varName) $ appE (fieldPeek field) addr)
+	let peekDec addr = do
+		headerDecs <- mapM (peekFieldDec addr) headerFields
+		let makeMatch (unionField, (matchPatStr, _, matchName)) = do
+			(_un, uc, ud) <- peekFieldDec addr unionField
+			match (conP (mkName matchPatStr) []) (normalB $ doE [ud, noBindS $ appE (varE 'return) $ recConE (mkName $ typeName ++ "_" ++ matchName) $ [c | (_n, c, _d) <- headerDecs] ++ [uc]]) []
+		let (sn, _sc, _sd) = headerDecs !! selectorIndex
+		let caseExp = caseE (varE sn) $ map makeMatch $ zip unionFields ufs
+		doE $ [d | (_n, _c, d) <- headerDecs] ++ [noBindS caseExp]
+
+	let pokeFieldDec addr field = do
+		varName <- newName $ "tmp_" ++ fieldNameStr field
+		return (return (fieldName field, VarP varName), noBindS $ appE (appE (fieldPoke field) addr) $ varE varName)
+	let pokeDec addr value = do
+		let makeMatch unionField = do
+			decs <- mapM (pokeFieldDec addr) $ headerFields ++ [unionField]
+			match (recP (mkName $ typeName ++ "_" ++ (fieldNameStr unionField)) $ map fst decs) (normalB $ doE $ map snd decs) []
+		caseE value $ map makeMatch unionFields
+
+	instanceDecs <- [d|
+		instance Storable $(conT $ mkName typeName) where
+			sizeOf _ = $(varE sizeOfName)
+			alignment _ = $(varE alignmentName)
+			peek addr = $(peekDec [| addr |])
+			poke addr value = $(pokeDec [| addr |] [| value |])
+		|]
+
+	return $ dataDec : headerFieldsDecs ++ unionFieldsDecs ++ structDecs ++ instanceDecs
