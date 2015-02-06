9fba4b56abe2dd3d9dcb5b20d2c1f928150b24cb
Quyse Lert <quyse0@gmail.com>
2015-02-06 00:46:00 +0000

    shaders work-in-progress

---
+++ flaw-graphics/Flaw/Graphics/Shading.hs
@@ -0,0 +1,14 @@
+{-|
+Module: Flaw.Graphics.Shading
+Description: Shader support.
+License: MIT
+-}
+
+module Flaw.Graphics.Shading
+	( ShaderableScalar()
+	, Shaderable()
+	, ShaderM(..)
+	, calc
+	) where
+
+import Flaw.Graphics.Shading.Internal
