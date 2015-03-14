e56c968d25f8d8e1c0d25a1cd2d7154cd1133183
Quyse Lert <quyse0@gmail.com>
2015-03-14 01:17:45 +0000

    new flaw-sl package

---
+++ flaw-sl/Flaw/Graphics/Program/SL.hs
@@ -0,0 +1,100 @@
+{-|
+Module: Flaw.Graphics.Program.SL
+Description: General helpers for HLSL/GLSL compilers.
+License: MIT
+-}
+
+{-# LANGUAGE GADTs #-}
+
+module Flaw.Graphics.Program.SL
+	( ProgramInfo(..)
+	, ShaderInfo
+	, programInfo
+	) where
+
+import Data.Array
+import Data.List
+
+import Flaw.Graphics.Program.Internal
+
+type NodeInfo = ([Int], [Attribute], [Uniform], [Sampler])
+
+nodeInfo :: Node a -> NodeInfo
+nodeInfo node = case node of
+	AttributeNode a -> ([], [a], [], [])
+	UniformNode u -> ([], [], [u], [])
+	TempNode t -> ([t], [], [], [])
+	ConstNode _ _ -> emptyNodeInfo
+	AddNode _ a b -> mergeNodeInfo (nodeInfo a) (nodeInfo b)
+	SubtractNode _ a b -> mergeNodeInfo (nodeInfo a) (nodeInfo b)
+	MultiplyNode _ a b -> mergeNodeInfo (nodeInfo a) (nodeInfo b)
+	DivideNode _ a b -> mergeNodeInfo (nodeInfo a) (nodeInfo b)
+	RecipNode _ a -> nodeInfo a
+	NegateNode _ a -> nodeInfo a
+	AbsNode _ a -> nodeInfo a
+	SignumNode _ a -> nodeInfo a
+	PiNode _ -> emptyNodeInfo
+	ExpNode _ a -> nodeInfo a
+	SqrtNode _ a -> nodeInfo a
+	LogNode _ a -> nodeInfo a
+	PowNode _ a b -> mergeNodeInfo (nodeInfo a) (nodeInfo b)
+	LogBaseNode _ a b -> mergeNodeInfo (nodeInfo a) (nodeInfo b)
+	SinNode _ a -> nodeInfo a
+	TanNode _ a -> nodeInfo a
+	CosNode _ a -> nodeInfo a
+	AsinNode _ a -> nodeInfo a
+	AtanNode _ a -> nodeInfo a
+	AcosNode _ a -> nodeInfo a
+	SinhNode _ a -> nodeInfo a
+	TanhNode _ a -> nodeInfo a
+	CoshNode _ a -> nodeInfo a
+	AsinhNode _ a -> nodeInfo a
+	AtanhNode _ a -> nodeInfo a
+	AcoshNode _ a -> nodeInfo a
+	MulNode _ _ _ a b -> mergeNodeInfo (nodeInfo a) (nodeInfo b)
+	DotNode _ _ a b -> mergeNodeInfo (nodeInfo a) (nodeInfo b)
+	InstanceIdNode -> ([], [], [], [])
+	ComponentNode _ _ _ a -> nodeInfo a
+	SwizzleNode _ _ _ a -> nodeInfo a
+	SampleNode (SamplerNode s) c -> mergeNodeInfo ([], [], [], [s]) (nodeInfo c)
+	CastNode _ _ a -> nodeInfo a
+	Combine2VecNode _ _ _ a b -> mergeNodeInfo (nodeInfo a) (nodeInfo b)
+	Combine3VecNode _ _ _ _ a b c -> mergeNodeInfo (nodeInfo a) $ mergeNodeInfo (nodeInfo b) (nodeInfo c)
+	Combine4VecNode _ _ _ _ _ a b c d -> mergeNodeInfo (nodeInfo a) $ mergeNodeInfo (nodeInfo b) $ mergeNodeInfo (nodeInfo c) (nodeInfo d)
+
+mergeNodeInfo :: NodeInfo -> NodeInfo -> NodeInfo
+mergeNodeInfo (t1, a1, u1, s1) (t2, a2, u2, s2) = (mergeSorted t1 t2, mergeSorted a1 a2, mergeSorted u1 u2, mergeSorted s1 s2)
+
+emptyNodeInfo :: NodeInfo
+emptyNodeInfo = ([], [], [], [])
+
+mergeSorted :: Ord a => [a] -> [a] -> [a]
+mergeSorted a [] = a
+mergeSorted [] a = a
+mergeSorted aa@(a:as) bb@(b:bs) = if a < b then a : mergeSorted as bb else b : mergeSorted aa bs
+
+unique :: Ord a => [a] -> [a]
+unique a = map head $ group a
+
+type ShaderInfo = ([Temp], [Attribute], [Uniform], [Sampler])
+
+shaderInfo :: [Temp] -> Array Int Temp -> ShaderInfo
+shaderInfo sourceTemps allTemps = totalInfo where
+	(tempIndices, attributes, uniforms, samplers) = foldr mergeNodeInfo emptyNodeInfo $ map (\Temp { tempNode = node } -> nodeInfo node) sourceTemps
+	temps = map (allTemps !) $ unique tempIndices
+	totalInfo = (temps, unique attributes, unique uniforms, unique samplers)
+
+data ProgramInfo
+	= VertexPixelProgramInfo ShaderInfo ShaderInfo
+
+-- | Generate shader programs in SL.
+programInfo :: State -> ProgramInfo
+programInfo State
+	{ stateTemps = temps
+	} = info where
+	allTemps = array (0, length temps - 1) $ map (\temp@Temp { tempIndex = i } -> (i, temp)) temps
+	vertexTemps = filter (\temp -> tempStage temp == VertexStage) temps
+	pixelTemps = filter (\temp -> tempStage temp == PixelStage) temps
+	vsInfo = shaderInfo vertexTemps allTemps
+	psInfo = shaderInfo pixelTemps allTemps
+	info = VertexPixelProgramInfo vsInfo psInfo
