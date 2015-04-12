3fb9e186ac97b5cb92c139f7c1f26097ac824200
Quyse Lert <quyse0@gmail.com>
2015-04-12 17:22:37 +0100

    packList helper

--- flaw-build/Flaw/Build.hs
+++ flaw-build/Flaw/Build.hs
@@ -10,11 +10,15 @@ module Flaw.Build
 	( loadFile
 	, Embed(..)
 	, fileExp
+	, packList
 	) where
 
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Unsafe as B
 import qualified Data.ByteString.Lazy as BL
+import Foreign.Marshal.Array
+import Foreign.Ptr
+import Foreign.Storable
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax(qAddDependentFile)
 
@@ -32,6 +36,9 @@ class Embed a where
 
 instance Embed Int where
 	embedExp n = [| return $(litE $ integerL $ fromIntegral n) |]
+
+instance Embed Bool where
+	embedExp b = [| return $(if b then (conE 'True) else (conE 'False)) |]
 
 instance Embed B.ByteString where
 	embedExp bytes = do
@@ -51,7 +58,32 @@ instance (Embed a, Embed b) => Embed (a, b) where
 		return (av, bv)
 		|]
 
+instance (Embed a, Embed b, Embed c) => Embed (a, b, c) where
+	embedExp (a, b, c) = [| do
+		av <- $(embedExp a)
+		bv <- $(embedExp b)
+		cv <- $(embedExp c)
+		return (av, bv, cv)
+		|]
+
+instance (Embed a, Embed b, Embed c, Embed d) => Embed (a, b, c, d) where
+	embedExp (a, b, c, d) = [| do
+		av <- $(embedExp a)
+		bv <- $(embedExp b)
+		cv <- $(embedExp c)
+		dv <- $(embedExp d)
+		return (av, bv, cv, dv)
+		|]
+
 -- | Embed file data as an expression.
 fileExp :: FilePath -> Q Exp
 fileExp filePath = do
 	embedExp =<< loadFile filePath
+
+-- | Pack storable list to bytestring.
+packList :: Storable a => [a] -> IO B.ByteString
+packList vs = do
+	let len = length vs
+	bytesPtr <- mallocArray len
+	pokeArray bytesPtr vs
+	B.unsafePackMallocCStringLen (castPtr bytesPtr, len * sizeOf (head vs))
