2bb2e5b168f9d97ea3d09a19e2d084a2723c7597
Quyse Lert <quyse0@gmail.com>
2015-04-26 00:44:51 +0100

    small rename

--- flaw-build/Flaw/Build.hs
+++ flaw-build/Flaw/Build.hs
@@ -22,7 +22,7 @@ import qualified Data.ByteString as B
 import qualified Data.ByteString.Unsafe as B
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text as T
-import qualified Data.Vector.Generic as V
+import qualified Data.Vector.Generic as VG
 import qualified Data.Vector.Storable as VS
 import Foreign.Marshal.Array
 import Foreign.Ptr
@@ -137,13 +137,13 @@ packList vs = do
 	B.unsafePackMallocCStringLen (castPtr bytesPtr, len * sizeOf (head vs))
 
 -- | Pack storable vector to bytestring.
-packVector :: (Storable a, V.Vector v a) => v a -> IO B.ByteString
+packVector :: (Storable a, VG.Vector v a) => v a -> IO B.ByteString
 packVector v = do
-	let len = V.length v
+	let len = VG.length v
 	bytesPtr <- mallocArray len
-	VS.unsafeWith (V.convert v) $ \vecPtr -> do
+	VS.unsafeWith (VG.convert v) $ \vecPtr -> do
 		copyArray bytesPtr vecPtr len
-	B.unsafePackMallocCStringLen (castPtr bytesPtr, len * sizeOf (V.head v))
+	B.unsafePackMallocCStringLen (castPtr bytesPtr, len * sizeOf (VG.head v))
 
 -- | Generate Embed instance for ADT.
 {- Example:
