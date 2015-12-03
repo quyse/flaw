8ed35440e5662b64565562e3fc654b2c22ebc0ad
Quyse Lert <quyse0@gmail.com>
2015-12-03 18:18:44 +0300

    first code of haskell implementation of oil protocol, flaw-oil library added

---
+++ flaw-oil/Flaw/Oil/ClientRepo.hs
@@ -0,0 +1,401 @@
+{-|
+Module: Flaw.Oil.ClientRepo
+Description: Oil client repo.
+License: MIT
+-}
+
+{-# LANGUAGE PatternSynonyms #-}
+
+module Flaw.Oil.ClientRepo
+	( ClientRepo()
+	, openClientRepo
+	, ClientRepoPushState(..)
+	, pushClientRepo
+	, ClientRepoPullInfo(..)
+	, pullClientRepo
+	) where
+
+import Control.Exception
+import Control.Monad
+import Data.Int
+import qualified Data.ByteString as B
+import qualified Data.Text as T
+import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vector.Unboxed.Mutable as VUM
+import Foreign.C.Types
+
+import Flaw.Book
+import Flaw.Data.Sqlite
+import Flaw.Exception
+import Flaw.Oil.Repo
+
+data ClientRepo = ClientRepo
+	{ clientRepoDb :: !SqliteDb
+	, clientRepoStmtManifestGet            :: !SqliteStmt
+	, clientRepoStmtManifestSet            :: !SqliteStmt
+	, clientRepoStmtGetKeyItems            :: !SqliteStmt
+	, clientRepoStmtGetKeyItemsByOneItemId :: !SqliteStmt
+	, clientRepoStmtGetKeyItemKey          :: !SqliteStmt
+	, clientRepoStmtGetKeyItemValue        :: !SqliteStmt
+	, clientRepoStmtGetKeyItemValueLength  :: !SqliteStmt
+	, clientRepoStmtAddKeyItem             :: !SqliteStmt
+	, clientRepoStmtRemoveKeyItem          :: !SqliteStmt
+	, clientRepoStmtChangeKeyItemStatus    :: !SqliteStmt
+	, clientRepoStmtChangeKeyItemValue     :: !SqliteStmt
+	, clientRepoStmtSelectKeysToPush       :: !SqliteStmt
+	, clientRepoStmtGetPushLag             :: !SqliteStmt
+	, clientRepoStmtMassChangeStatus       :: !SqliteStmt
+	, clientRepoStmtEnumerateKeysBegin     :: !SqliteStmt
+	, clientRepoStmtEnumerateKeysBeginEnd  :: !SqliteStmt
+	, clientRepoStmtAddChunk               :: !SqliteStmt
+	, clientRepoStmtPreCutChunks           :: !SqliteStmt
+	, clientRepoStmtCutChunks              :: !SqliteStmt
+	, clientRepoStmtGetUpperRevision       :: !SqliteStmt
+	}
+
+openClientRepo :: T.Text -> IO (ClientRepo, IO ())
+openClientRepo fileName = describeException "failed to open oil client repo" $ withSpecialBook $ \bk -> do
+	-- open db
+	db <- book bk $ openRepoDb fileName clientRepoVersion
+
+	-- enable normal synchronous mode (db correctness is a concern, but durability is not for client repo)
+	sqliteExec db $ T.pack "PRAGMA synchronous = NORMAL"
+
+	-- ensure tables and indices exist
+
+	-- manifest table
+	sqliteExec db $ T.pack
+		"CREATE TABLE IF NOT EXISTS manifest (\
+		\key INTEGER PRIMARY KEY, \
+		\value ANY NOT NULL)"
+	-- items table
+	sqliteExec db $ T.pack
+		"CREATE TABLE IF NOT EXISTS items (\
+		\id INTEGER PRIMARY KEY, \
+		\value BLOB NOT NULL, \
+		\key BLOB NOT NULL, \
+		\status INTEGER NOT NULL)"
+	-- items_key_status index
+	sqliteExec db $ T.pack
+		"CREATE UNIQUE INDEX IF NOT EXISTS items_key_status ON items (key, status)"
+	-- items_status_partial index
+	sqliteExec db $ T.pack $
+		"CREATE INDEX IF NOT EXISTS items_status_partial ON items (status) WHERE\
+		\ status = " ++ show (ItemStatusClient :: Int) ++
+		" OR status = " ++ show (ItemStatusTransient :: Int) ++
+		" OR status = " ++ show (ItemStatusPostponed :: Int)
+	-- chunks table
+	sqliteExec db $ T.pack
+		"CREATE TABLE IF NOT EXISTS chunks (\
+		\prerev INTEGER PRIMARY KEY, \
+		\postrev INTEGER NOT NULL)"
+
+	-- create statements
+	let createStmt str = book bk $ sqliteStmt db $ T.pack str
+	stmtManifestGet            <- createStmt "SELECT value FROM manifest WHERE key = ?1"
+	stmtManifestSet            <- createStmt "INSERT OR REPLACE INTO manifest (key, value) VALUES (?1, ?2)"
+	stmtGetKeyItems            <- createStmt "SELECT id, status FROM items WHERE key = ?1"
+	stmtGetKeyItemsByOneItemId <- createStmt "SELECT id, status FROM items WHERE key = (SELECT key FROM items WHERE id = ?1)"
+	stmtGetKeyItemKey          <- createStmt "SELECT key FROM items WHERE id = ?1"
+	stmtGetKeyItemValue        <- createStmt "SELECT value FROM items WHERE id = ?1"
+	stmtGetKeyItemValueLength  <- createStmt "SELECT LENGTH(value) FROM items WHERE id = ?1"
+	stmtAddKeyItem             <- createStmt "INSERT OR REPLACE INTO items (key, value, status) VALUES (?1, ?2, ?3)"
+	stmtRemoveKeyItem          <- createStmt "DELETE FROM items WHERE id = ?1"
+	stmtChangeKeyItemStatus    <- createStmt "UPDATE OR REPLACE items SET status = ?2 WHERE id = ?1"
+	stmtChangeKeyItemValue     <- createStmt "UPDATE items SET value = ?2 WHERE id = ?1"
+	stmtSelectKeysToPush       <- createStmt $ "SELECT id, key, value FROM items WHERE status = " ++ show (ItemStatusClient :: Int) ++ " ORDER BY id LIMIT ?1"
+	stmtGetPushLag             <- createStmt $ "SELECT COUNT(*) FROM items WHERE status = " ++ show (ItemStatusClient :: Int)
+	stmtMassChangeStatus       <- createStmt "UPDATE OR REPLACE items SET status = ?2 WHERE status = ?1"
+	stmtEnumerateKeysBegin     <- createStmt "SELECT DISTINCT key FROM items WHERE key >= ?1 ORDER BY key"
+	stmtEnumerateKeysBeginEnd  <- createStmt "SELECT DISTINCT key FROM items WHERE key >= ?1 AND key < ?2 ORDER BY key"
+	stmtAddChunk               <- createStmt "INSERT INTO chunks (prerev, postrev) VALUES (?1, ?2)"
+	stmtPreCutChunks           <- createStmt "SELECT MAX(postrev) FROM chunks WHERE prerev <= ?1"
+	stmtCutChunks              <- createStmt "DELETE FROM chunks WHERE prerev <= ?1"
+	stmtGetUpperRevision       <- createStmt "SELECT MIN(prerev) FROM chunks"
+
+	return ClientRepo
+		{ clientRepoDb = db
+		, clientRepoStmtManifestGet            = stmtManifestGet
+		, clientRepoStmtManifestSet            = stmtManifestSet
+		, clientRepoStmtGetKeyItems            = stmtGetKeyItems
+		, clientRepoStmtGetKeyItemsByOneItemId = stmtGetKeyItemsByOneItemId
+		, clientRepoStmtGetKeyItemKey          = stmtGetKeyItemKey
+		, clientRepoStmtGetKeyItemValue        = stmtGetKeyItemValue
+		, clientRepoStmtGetKeyItemValueLength  = stmtGetKeyItemValueLength
+		, clientRepoStmtAddKeyItem             = stmtAddKeyItem
+		, clientRepoStmtRemoveKeyItem          = stmtRemoveKeyItem
+		, clientRepoStmtChangeKeyItemStatus    = stmtChangeKeyItemStatus
+		, clientRepoStmtChangeKeyItemValue     = stmtChangeKeyItemValue
+		, clientRepoStmtSelectKeysToPush       = stmtSelectKeysToPush
+		, clientRepoStmtGetPushLag             = stmtGetPushLag
+		, clientRepoStmtMassChangeStatus       = stmtMassChangeStatus
+		, clientRepoStmtEnumerateKeysBegin     = stmtEnumerateKeysBegin
+		, clientRepoStmtEnumerateKeysBeginEnd  = stmtEnumerateKeysBeginEnd
+		, clientRepoStmtAddChunk               = stmtAddChunk
+		, clientRepoStmtPreCutChunks           = stmtPreCutChunks
+		, clientRepoStmtCutChunks              = stmtCutChunks
+		, clientRepoStmtGetUpperRevision       = stmtGetUpperRevision
+		}
+
+-- Item statuses.
+-- server version
+pattern ItemStatusServer = 0
+-- client change based on 'server' in case of no conflict
+pattern ItemStatusClient = 1
+-- client change based on 'server' which is in process of committing to server
+pattern ItemStatusTransient = 2
+-- client change based on 'transient', waiting for results of commit of 'transient'
+pattern ItemStatusPostponed = 3
+-- count
+pattern ItemStatusesCount = 4
+
+-- Manifest keys.
+pattern ManifestKeyGlobalRevision = 0
+
+type ItemId = Int64
+
+getManifestValue :: ClientRepo -> CInt -> Int64 -> IO Int64
+getManifestValue ClientRepo
+	{ clientRepoStmtManifestGet = stmtManifestGet
+	} key defaultValue = do
+	sqliteQuery stmtManifestGet $ \query -> do
+		sqliteBind query 1 key
+		r <- sqliteStep query
+		if r then sqliteColumn query 0
+		else return defaultValue
+
+setManifestValue :: ClientRepo -> CInt -> Int64 -> IO ()
+setManifestValue ClientRepo
+	{ clientRepoStmtManifestSet = stmtManifestSet
+	} key value = do
+	sqliteQuery stmtManifestSet $ \query -> do
+		sqliteBind query 1 key
+		sqliteBind query 2 value
+		sqliteFinalStep query
+
+-- | Get global revision in client repo.
+-- Tries to increase global revision found in manifest, by using chunks, and then removes those chunks.
+getGlobalRevision :: ClientRepo -> IO Revision
+getGlobalRevision repo@ClientRepo
+	{ clientRepoStmtPreCutChunks = stmtPreCutChunks
+	, clientRepoStmtCutChunks = stmtCutChunks
+	} = do
+	-- get global revision from manifest, and try to increase it using chunks
+	let preCutChunks globalRevision = do
+		preCutRevision <- sqliteQuery stmtPreCutChunks $ \query -> do
+			sqliteBind query 1 globalRevision
+			r <- sqliteStep query
+			when (not r) $ throwIO $ DescribeFirstException "failed to get pre-cut revision"
+			sqliteColumn query 0
+		-- try again if it actually increased
+		if (preCutRevision > globalRevision) then preCutChunks preCutRevision
+		else return globalRevision
+	firstGlobalRevision <- getManifestValue repo ManifestKeyGlobalRevision 0
+	globalRevision <- preCutChunks firstGlobalRevision
+	-- if global revision has actually incresed, remember it in manifest
+	when (globalRevision > firstGlobalRevision) $ setManifestValue repo ManifestKeyGlobalRevision globalRevision
+	-- remove chunks behind global revision
+	sqliteQuery stmtCutChunks $ \query -> do
+		sqliteBind query 1 globalRevision
+		sqliteFinalStep query
+	return globalRevision
+
+addKeyItem :: ClientRepo -> B.ByteString -> B.ByteString -> CInt -> IO ()
+addKeyItem ClientRepo
+	{ clientRepoStmtAddKeyItem = stmtAddKeyItem
+	} key value status = sqliteQuery stmtAddKeyItem $ \query -> do
+	sqliteBind query 1 key
+	sqliteBind query 2 value
+	sqliteBind query 3 status
+	sqliteFinalStep query
+
+removeKeyItem :: ClientRepo -> ItemId -> IO ()
+removeKeyItem ClientRepo
+	{ clientRepoStmtRemoveKeyItem = stmtRemoveKeyItem
+	} itemId = sqliteQuery stmtRemoveKeyItem $ \query -> do
+	sqliteBind query 1 itemId
+	sqliteFinalStep query
+
+changeKeyItemStatus :: ClientRepo -> ItemId -> CInt -> IO ()
+changeKeyItemStatus ClientRepo
+	{ clientRepoStmtChangeKeyItemStatus = stmtChangeKeyItemStatus
+	} itemId newStatus = sqliteQuery stmtChangeKeyItemStatus $ \query -> do
+	sqliteBind query 1 itemId
+	sqliteBind query 2 newStatus
+	sqliteFinalStep query
+
+getKeyItemValueLength :: ClientRepo -> ItemId -> IO Int64
+getKeyItemValueLength ClientRepo
+	{ clientRepoStmtGetKeyItemValueLength = stmtGetKeyItemValueLength
+	} itemId = sqliteQuery stmtGetKeyItemValueLength $ \query -> do
+	sqliteBind query 1 itemId
+	r <- sqliteStep query
+	when (not r) $ throwIO $ DescribeFirstException "failed to get key item value length"
+	sqliteColumn query 0
+
+changeKeyItemValue :: ClientRepo -> ItemId -> B.ByteString -> IO ()
+changeKeyItemValue ClientRepo
+	{ clientRepoStmtChangeKeyItemValue = stmtChangeKeyItemValue
+	} itemId newValue = sqliteQuery stmtChangeKeyItemValue $ \query -> do
+	sqliteBind query 1 itemId
+	sqliteBind query 2 newValue
+	sqliteFinalStep query
+
+-- | Collection of items with the same key.
+newtype KeyItems = KeyItems (VU.Vector ItemId)
+
+fillKeyItems :: SqliteQuery -> IO KeyItems
+fillKeyItems query = do
+	v <- VUM.replicate ItemStatusesCount 0
+	let step = do
+		r <- sqliteStep query
+		when r $ do
+			itemStatus <- sqliteColumn query 1
+			when (itemStatus < 0 || itemStatus >= ItemStatusesCount) $ throwIO $ DescribeFirstException "wrong item status"
+			itemId <- sqliteColumn query 0
+			VUM.write v (fromIntegral (itemStatus :: CInt)) itemId
+			step
+	step
+	liftM KeyItems $ VU.unsafeFreeze v
+
+getKeyItems :: ClientRepo -> B.ByteString -> IO KeyItems
+getKeyItems ClientRepo
+	{ clientRepoStmtGetKeyItems = stmtGetKeyItems
+	} key = sqliteQuery stmtGetKeyItems $ \query -> do
+	sqliteBind query 1 key
+	fillKeyItems query
+
+getKeyItemsByOneItemId :: ClientRepo -> ItemId -> IO KeyItems
+getKeyItemsByOneItemId ClientRepo
+	{ clientRepoStmtGetKeyItemsByOneItemId = stmtGetKeyItemsByOneItemId
+	} itemId = sqliteQuery stmtGetKeyItemsByOneItemId $ \query -> do
+	sqliteBind query 1 itemId
+	fillKeyItems query
+
+-- | State of push, needed for pull.
+newtype ClientRepoPushState = ClientRepoPushState
+	{ clientRepoPushStateTransientIds :: [ItemId]
+	}
+
+-- | Perform push.
+pushClientRepo :: ClientRepo -> Manifest -> IO (Push, ClientRepoPushState)
+pushClientRepo repo@ClientRepo
+	{ clientRepoDb = db
+	, clientRepoStmtSelectKeysToPush = stmtSelectKeysToPush
+	, clientRepoStmtGetUpperRevision = stmtGetUpperRevision
+	} Manifest
+	{ manifestMaxPushItemsCount = maxPushItemsCount
+	, manifestMaxPushValuesTotalSize = maxPushValuesTotalSize
+	} = describeException "failed to push client repo" $ sqliteTransaction db $ \commit -> do
+
+	-- get global revision
+	clientRevision <- getGlobalRevision repo
+
+	-- get upper revision
+	clientUpperRevision <- sqliteQuery stmtGetUpperRevision $ \query -> do
+		r <- sqliteStep query
+		when (not r) $ throwIO $ DescribeFirstException "failed to get upper revision"
+		sqliteColumn query 0
+
+	-- select keys to push
+	(items, transientIds) <- sqliteQuery stmtSelectKeysToPush $ \query -> do
+		sqliteBind query 1 (fromIntegral maxPushItemsCount :: Int64)
+		let step pushValuesTotalSize = do
+			-- get next row
+			r <- sqliteStep query
+			if r then do
+				-- get value
+				value <- sqliteColumn query 2
+				-- check limits
+				let newPushValuesTotalSize = pushValuesTotalSize + B.length value
+				if newPushValuesTotalSize > maxPushValuesTotalSize then return ([], [])
+				else do
+					-- get key
+					key <- sqliteColumn query 1
+					-- get id of item
+					itemId <- sqliteColumn query 0
+					-- change status of item to 'transient'
+					changeKeyItemStatus repo itemId ItemStatusTransient
+					-- get rest of the items and return
+					(restItems, restItemIds) <- step newPushValuesTotalSize
+					return ((key, value) : restItems, itemId : restItemIds)
+			else return ([], [])
+		step 0
+
+	-- commit and return
+	commit
+
+	return (Push
+		{ pushClientRevision = clientRevision
+		, pushClientUpperRevision = clientUpperRevision
+		, pushItems = items
+		}, ClientRepoPushState
+		{ clientRepoPushStateTransientIds = transientIds
+		})
+
+data ClientRepoPullInfo = ClientRepoPullInfo
+	{ clientRepoPullLag :: !Int64
+	, clientRepoPullChanges :: [(B.ByteString, B.ByteString)]
+	}
+
+pullClientRepo :: ClientRepo -> Pull -> ClientRepoPushState -> IO ClientRepoPullInfo
+pullClientRepo repo@ClientRepo
+	{ clientRepoDb = db
+	, clientRepoStmtAddChunk = stmtAddChunk
+	} Pull
+	{ pullLag = lag
+	, pullPrePushRevision = prePushRevision
+	, pullPostPushRevision = postPushRevision
+	, pullItems = itemsToPull
+	, pullNewClientRevision = newClientRevision
+	} ClientRepoPushState
+	{ clientRepoPushStateTransientIds = transientIds
+	} = describeException "failed to pull client repo" $ sqliteTransaction db $ \commit -> do
+
+	-- process commited keys
+	forM_ transientIds $ \transientItemId -> do
+		-- get key items
+		KeyItems keyItemIds <- getKeyItemsByOneItemId repo transientItemId
+
+		-- 'transient' becomes 'server'
+		transientValueLength <- getKeyItemValueLength repo transientItemId
+		if transientValueLength > 0 then changeKeyItemStatus repo transientItemId ItemStatusServer
+		else do
+			removeKeyItem repo transientItemId
+			let serverItemId = keyItemIds VU.! ItemStatusServer
+			when (serverItemId > 0) $ removeKeyItem repo serverItemId
+		-- 'postponed' (if presented) becomes 'client'
+		let postponedItemId = keyItemIds VU.! ItemStatusPostponed
+		when (postponedItemId > 0) $ changeKeyItemStatus repo postponedItemId ItemStatusClient
+
+	-- add chunk if something has been committed
+	when (prePushRevision < postPushRevision) $ do
+		sqliteQuery stmtAddChunk $ \query -> do
+			sqliteBind query 1 prePushRevision
+			sqliteBind query 2 postPushRevision
+			sqliteFinalStep query
+
+	-- pull keys
+	forM_ itemsToPull $ \(key, value) -> do
+		-- get key items
+		KeyItems keyItemIds <- getKeyItems repo key
+		-- value always becomes 'server'
+		-- see what we need to do
+		let serverItemId = keyItemIds VU.! ItemStatusServer
+		if serverItemId > 0 then
+			if B.length value > 0 then
+				changeKeyItemValue repo serverItemId value
+			else
+				removeKeyItem repo serverItemId
+		else when (B.length value > 0) $ addKeyItem repo key value ItemStatusServer
+
+	-- set new client revision
+	setManifestValue repo ManifestKeyGlobalRevision newClientRevision
+
+	-- commit and return
+	commit
+
+	return ClientRepoPullInfo
+		{ clientRepoPullLag = lag
+		, clientRepoPullChanges = itemsToPull
+		}
---
+++ flaw-oil/Flaw/Oil/Repo.hs
@@ -0,0 +1,166 @@
+{-|
+Module: Flaw.Oil.Repo
+Description: General repo functions.
+License: MIT
+-}
+
+{-# LANGUAGE DeriveGeneric #-}
+
+module Flaw.Oil.Repo
+	( RepoVersion(..)
+	, serverRepoVersion
+	, clientRepoVersion
+	, Manifest(..)
+	, protocolVersion
+	, defaultRepoManifest
+	, openRepoDb
+	, Revision
+	, UserId
+	, Push(..)
+	, Pull(..)
+	, checkPushLimits
+	, SyncError(..)
+	) where
+
+import Control.Exception
+import Control.Monad
+import qualified Data.ByteString as B
+import Data.Foldable
+import Data.Int
+import qualified Data.Text as T
+import Foreign.C.Types
+import GHC.Generics
+
+import Flaw.Book
+import Flaw.Data.Sqlite
+import Flaw.Exception
+
+newtype RepoVersion = RepoVersion CInt
+
+-- | "application_id" in SQLite db of server repo.
+-- Convention: oil<letter starting from A> in little-endian.
+serverRepoVersion :: RepoVersion
+serverRepoVersion = RepoVersion 0x416c696f -- "oilA"
+
+-- | "application_id" in SQLite db of client repo.
+-- Convention: oil<letter starting from a> in little-endian.
+clientRepoVersion :: RepoVersion
+clientRepoVersion = RepoVersion 0x616c696f -- "oila"
+
+-- | Manifest with most limitations.
+data Manifest = Manifest
+	{ manifestProtocolVersion :: !Int32
+	, manifestMaxKeySize :: !Int
+	, manifestMaxValueSize :: !Int
+	, manifestMaxPushItemsCount :: !Int
+	, manifestMaxPushValuesTotalSize :: !Int
+	, manifestMaxPullItemsCount :: !Int
+	, manifestMaxPullValuesTotalSize :: !Int
+	}
+
+-- | Current protocol version.
+-- Convention: oil<digit starting from 0> in little-endian.
+protocolVersion :: Int32
+protocolVersion = 0x306c696f -- "oil0"
+
+defaultRepoManifest :: Manifest
+defaultRepoManifest = Manifest
+	{ manifestProtocolVersion = protocolVersion
+	, manifestMaxKeySize = 128
+	, manifestMaxValueSize = 1024 * 1024
+	, manifestMaxPushItemsCount = 1024
+	, manifestMaxPushValuesTotalSize = 1024 * 1024 * 2
+	, manifestMaxPullItemsCount = 1024
+	, manifestMaxPullValuesTotalSize = 1024 * 1024 * 2
+	}
+
+openRepoDb :: T.Text -> RepoVersion -> IO (SqliteDb, IO ())
+openRepoDb fileName (RepoVersion version) = withSpecialBook $ \bk -> do
+	-- open db
+	db <- book bk $ sqliteDb fileName
+
+	-- enable exclusive locking mode
+	sqliteExec db $ T.pack "PRAGMA locking_mode = EXCLUSIVE"
+	-- enable WAL journal mode
+	sqliteExec db $ T.pack "PRAGMA journal_mode = WAL"
+
+	-- check version
+	withBook $ \tempBk -> do
+		stmt <- book tempBk $ sqliteStmt db $ T.pack "PRAGMA application_id"
+		sqliteQuery stmt $ \query -> do
+			r <- sqliteStep query
+			when (not r) $ throwIO $ DescribeFirstException "failed to get application_id"
+			currentAppVersion <- sqliteColumn query 0
+			-- if version is not set yet
+			if currentAppVersion == 0 then do
+				-- set it
+				sqliteExec db $ T.pack $ "PRAGMA application_id = " ++ show version
+			-- else check that version is correct
+			else when (currentAppVersion /= version) $ throwIO $ DescribeFirstException "wrong application_id"
+
+	return db
+
+-- | Type for revisions.
+-- Start revision is 1. 0 means no revisions.
+type Revision = Int64
+
+-- | Type for user ID.
+type UserId = Int64
+
+-- | Data sent by client to server.
+data Push = Push
+	{
+	-- | Current revision of the client.
+	  pushClientRevision :: !Revision
+	-- | Upper bound on revisions server may send to client.
+	-- Used to prevent sending revisions client already knows about.
+	, pushClientUpperRevision :: !Revision
+	-- | Pushed (key, value) pairs.
+	, pushItems :: [(B.ByteString, B.ByteString)]
+	}
+
+-- | Data sent by server to client.
+data Pull = Pull
+	{
+	-- | Total number of revisions client needs to pull in order to catch up with server, counting from pushClientRevision
+	  pullLag :: !Int64
+	-- | Server revision before pushing items.
+	, pullPrePushRevision :: !Revision
+	-- | Server revision after pushing items (should be equal to pre-push revision + number of items pushed).
+	, pullPostPushRevision :: !Revision
+	-- | Pairs (key, value) to pull.
+	, pullItems :: [(B.ByteString, B.ByteString)]
+	-- | New client revision after whole operation.
+	, pullNewClientRevision :: !Revision
+	} deriving Generic
+
+-- | Check push limits.
+checkPushLimits :: Manifest -> Push -> Maybe SyncError
+checkPushLimits Manifest
+	{ manifestMaxKeySize = maxKeySize
+	, manifestMaxValueSize = maxValueSize
+	, manifestMaxPushItemsCount = maxPushItemsCount
+	, manifestMaxPushValuesTotalSize = maxPushValuesTotalSize
+	} Push
+	{ pushItems = items
+	} = maybeError where
+	maybeError =
+		if length items > maxPushItemsCount then Just SyncTooManyItemsError
+		else case foldrM f 0 items of
+			Right _valuesTotalSize -> Nothing
+			Left err -> Just err
+	f (key, value) valuesTotalSize =
+		if B.length key > maxKeySize then Left SyncTooBigKeyError
+		else if B.length value > maxValueSize then Left SyncTooBigValueError
+		else let newValuesTotalSize = valuesTotalSize + B.length value in
+			if newValuesTotalSize > maxPushValuesTotalSize then Left SyncTooBigPushValuesTotalSize
+			else Right newValuesTotalSize
+
+-- | Errors while syncing (reported to client).
+data SyncError
+	= SyncTooManyItemsError
+	| SyncTooBigKeyError
+	| SyncTooBigValueError
+	| SyncTooBigPushValuesTotalSize
+	| SyncFatalError
+	deriving Generic
