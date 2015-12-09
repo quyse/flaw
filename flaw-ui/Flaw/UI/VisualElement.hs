dc353034734ebbbdd3ded7078b9f2432a6ad345c
Quyse Lert <quyse0@gmail.com>
2015-12-09 23:19:12 +0300

    VisualElement ui element

---
+++ flaw-ui/Flaw/UI/VisualElement.hs
@@ -0,0 +1,49 @@
+{-|
+Module: Flaw.UI.VisualElement
+Description: Adapter for Visual to use it as Element.
+License: MIT
+-}
+
+module Flaw.UI.VisualElement
+	( VisualElement(..)
+	, newVisualElement
+	) where
+
+import Control.Concurrent.STM
+
+import Flaw.Math
+import Flaw.UI
+import Flaw.UI.Drawer
+
+data VisualElement = VisualElement
+	{ visualElementVisual :: !SomeVisual
+	, visualElementSizeVar :: !(TVar Size)
+	}
+
+newVisualElement :: Visual v => v -> STM VisualElement
+newVisualElement visual = do
+	sizeVar <- newTVar $ Vec2 0 0
+	return VisualElement
+		{ visualElementVisual = SomeVisual visual
+		, visualElementSizeVar = sizeVar
+		}
+
+instance Element VisualElement where
+	layoutElement VisualElement
+		{ visualElementSizeVar = sizeVar
+		} size = writeTVar sizeVar size
+	dabElement _ _ = return False
+	elementMouseCursor _ = return MouseCursorArrow
+	renderElement VisualElement
+		{ visualElementVisual = SomeVisual visual
+		, visualElementSizeVar = sizeVar
+		} drawer@Drawer
+		{ drawerStyles = DrawerStyles
+			{ drawerFlatStyleVariant = StyleVariant
+				{ styleVariantNormalStyle = style
+				}
+			}
+		} position = do
+		size <- readTVar sizeVar
+		renderVisual visual drawer position size style
+	processInputEvent _ _ _ = return False
