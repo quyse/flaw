54173a3c5325a9841934483bafb08908529e39d9
Quyse Lert <quyse0@gmail.com>
2016-03-02 11:49:21 +0300

    remove debug logging

--- flaw-asset/js/flaw-asset.js
+++ flaw-asset/js/flaw-asset.js
@@ -3,7 +3,6 @@ function h$flaw_asset_get_asset(url, callback) {
 	xhr.open('GET', url, true);
 	xhr.responseType = 'arraybuffer';
 	xhr.onload = function(e) {
-		console.log(this);
 		callback(this.status == 200 ? this.response : new ArrayBuffer(0));
 	};
 	xhr.send();
