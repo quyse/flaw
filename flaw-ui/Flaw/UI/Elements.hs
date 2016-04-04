4323b4027b9644119e47bdc62cb80eef6d24e5ff
Quyse Lert <quyse0@gmail.com>
2016-04-04 19:47:29 +0300

    re-export all ui elements in a single module

---
+++ flaw-ui/Flaw/UI/Elements.hs
@@ -0,0 +1,25 @@
+{-|
+Module: Flaw.UI.Elements
+Description: All UI elements.
+License: MIT
+-}
+
+module Flaw.UI.Elements
+	( module Flaw.UI.Button
+	, module Flaw.UI.EditBox
+	, module Flaw.UI.Frame
+	, module Flaw.UI.Label
+	, module Flaw.UI.Panel
+	, module Flaw.UI.Slider
+	, module Flaw.UI.VisualElement
+	, module Flaw.UI.Window
+	) where
+
+import Flaw.UI.Button
+import Flaw.UI.EditBox
+import Flaw.UI.Frame
+import Flaw.UI.Label
+import Flaw.UI.Panel
+import Flaw.UI.Slider
+import Flaw.UI.VisualElement
+import Flaw.UI.Window
