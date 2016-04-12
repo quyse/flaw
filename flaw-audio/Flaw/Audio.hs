436af7ebf01e8e274ab35ba0d9a0f919a3774cfc
Quyse Lert <quyse0@gmail.com>
2016-04-12 17:15:21 +0300

    new flaw-audio package

---
+++ flaw-audio/Flaw/Audio.hs
@@ -0,0 +1,45 @@
+{-|
+Module: Flaw.Audio
+Description: Audio abstraction.
+License: MIT
+-}
+
+{-# LANGUAGE TypeFamilies #-}
+
+module Flaw.Audio
+	( Device(..)
+	, SoundFormat(..)
+	) where
+
+import qualified Data.ByteString as B
+
+import Flaw.Math
+
+class Device d where
+	-- | Sound type.
+	data SoundId d :: *
+	-- | Type of sound player.
+	data SoundPlayerId d :: *
+	-- | Create buffered sound.
+	createSound :: d -> SoundFormat -> B.ByteString -> IO (SoundId d, IO ())
+	-- | Create player for a sound.
+	createSoundPlayer :: SoundId d -> IO (SoundPlayerId d, IO ())
+	-- | Start playing.
+	playSound :: SoundPlayerId d -> IO ()
+	-- | Pause playing.
+	pauseSound :: SoundPlayerId d -> IO ()
+	-- | Stop playing.
+	stopSound :: SoundPlayerId d -> IO ()
+	-- | Set position of sound player.
+	setSoundPosition :: SoundPlayerId d -> Float3 -> IO ()
+	-- | Set direction of sound player.
+	setSoundDirection :: SoundPlayerId d -> Float3 -> IO ()
+	-- | Set velocity of sound player.
+	setSoundVelocity :: SoundPlayerId d -> Float3 -> IO ()
+
+-- | Sound format type.
+data SoundFormat = SoundFormat
+	{ soundFormatSamplesPerSecond :: {-# UNPACK #-} !Int
+	, soundFormatBitsPerSample :: {-# UNPACK #-} !Int
+	, soundFormatChannelsCount :: {-# UNPACK #-} !Int
+	}
