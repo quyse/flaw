e210d6f78fd5c724a19c85d9c000568db8ab102f
Quyse Lert <quyse0@gmail.com>
2016-04-14 15:10:38 +0300

    initial ffmpeg integration

---
+++ flaw-ffmpeg/Flaw/Media/FFmpeg.hs
@@ -0,0 +1,126 @@
+{-|
+Module: Flaw.Media.FFmpeg
+Description: FFmpeg integration.
+License: MIT
+-}
+
+{-# LANGUAGE PatternSynonyms #-}
+
+module Flaw.Media.FFmpeg
+	( FFmpegAVFormatContext()
+	, ffmpegInit
+	, ffmpegOpenAVFormatContext
+	, ffmpegDecodeAudio
+	) where
+
+import Control.Exception
+import Control.Concurrent
+import Control.Monad
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
+import Data.IORef
+import Data.Monoid
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import Foreign.C.Types
+import Foreign.Marshal.Alloc
+import Foreign.Marshal.Utils
+import Foreign.Ptr
+import Foreign.Storable
+
+import Flaw.Audio
+import Flaw.Book
+import Flaw.Exception
+
+newtype FFmpegAVFormatContext = FFmpegAVFormatContext (Ptr C_AVFormatContext)
+
+ffmpegInit :: IO ()
+ffmpegInit = flaw_ffmpeg_init
+
+-- | Open AV format context by media URL.
+ffmpegOpenAVFormatContext :: T.Text -> IO (FFmpegAVFormatContext, IO ())
+ffmpegOpenAVFormatContext url = describeException "failed to open FFmpeg AV format context" $ withSpecialBook $ \bk -> do
+
+	-- open AV format context
+	formatContextPtr <- runInBoundThread $ B.useAsCString (T.encodeUtf8 url) flaw_ffmpeg_openInput
+	when (formatContextPtr == nullPtr) $ throwIO $ DescribeFirstException "failed to open input"
+	book bk $ return ((), with formatContextPtr avformat_close_input)
+
+	-- find stream info
+	checkAVError "avformat_find_stream_info" $ avformat_find_stream_info formatContextPtr nullPtr
+
+	return $ FFmpegAVFormatContext formatContextPtr
+
+ffmpegDecodeAudio :: FFmpegAVFormatContext -> IO (SoundFormat, BL.ByteString)
+ffmpegDecodeAudio (FFmpegAVFormatContext formatContextPtr) = describeException "failed to decode ffmpeg audio" $ do
+	-- get audio stream
+	streamPtr <- flaw_ffmpeg_getSingleAudioStream formatContextPtr
+	when (streamPtr == nullPtr) $ throwIO $ DescribeFirstException "no single audio stream"
+	-- find codec
+	checkAVError "prepare decoding" $ flaw_ffmpeg_prepareStreamDecoding streamPtr
+	-- decode
+	chunksRef <- newIORef []
+	outputCallback <- wrapOutputCallback $ \chunkPtr chunkLen ->
+		modifyIORef' chunksRef =<< (:) <$> B.packCStringLen (chunkPtr, fromIntegral chunkLen)
+	format <- alloca $ \samplesPerSecondPtr -> alloca $ \sampleFormatPtr -> alloca $ \channelsCountPtr -> do
+		checkAVError "decode audio" $
+			flaw_ffmpeg_decodeAudio formatContextPtr streamPtr outputCallback
+				samplesPerSecondPtr sampleFormatPtr channelsCountPtr
+		-- get sound format
+		SoundFormat
+			<$> (fromIntegral <$> peek samplesPerSecondPtr)
+			<*> (convertSampleFormat <$> peek sampleFormatPtr)
+			<*> (fromIntegral <$> peek channelsCountPtr)
+	-- get samples data
+	bytes <- BL.fromChunks . reverse <$> readIORef chunksRef
+	return (format, bytes)
+
+checkAVError :: String -> IO CInt -> IO ()
+checkAVError message action = do
+	r <- action
+	unless (r == 0) $ throwIO $ DescribeFirstException $ "AV error " <> show r <> ": " <> message
+
+convertSampleFormat :: CInt -> SoundSampleType
+convertSampleFormat sampleFormat = case sampleFormat of
+	AV_SAMPLE_FMT_U8 -> SoundSampleByte
+	AV_SAMPLE_FMT_S16 -> SoundSampleShort
+	AV_SAMPLE_FMT_S32 -> SoundSampleInt
+	AV_SAMPLE_FMT_FLT -> SoundSampleFloat
+	AV_SAMPLE_FMT_DBL -> SoundSampleDouble
+	_ -> error "unknown AV sample format"
+
+-- flaw-ffmpeg helper routines
+
+foreign import ccall safe flaw_ffmpeg_init :: IO ()
+foreign import ccall safe flaw_ffmpeg_openInput :: Ptr CChar -> IO (Ptr C_AVFormatContext)
+foreign import ccall safe flaw_ffmpeg_getSingleVideoStream :: Ptr C_AVFormatContext -> IO (Ptr C_AVStream)
+foreign import ccall safe flaw_ffmpeg_getSingleAudioStream :: Ptr C_AVFormatContext -> IO (Ptr C_AVStream)
+foreign import ccall safe flaw_ffmpeg_prepareStreamDecoding :: Ptr C_AVStream -> IO CInt
+foreign import ccall safe flaw_ffmpeg_decodeAudio
+	:: Ptr C_AVFormatContext -> Ptr C_AVStream -> FunPtr OutputCallback -> Ptr CInt -> Ptr CInt -> Ptr CInt -> IO CInt
+
+-- wrappers
+
+type OutputCallback = Ptr CChar -> CInt -> IO ()
+foreign import ccall "wrapper" wrapOutputCallback :: OutputCallback -> IO (FunPtr OutputCallback)
+
+-- ffmpeg types
+
+data C_AVFormatContext
+data C_AVStream
+data C_AVDictionary
+
+-- ffmpeg routines
+
+foreign import ccall safe avformat_close_input
+	:: Ptr (Ptr C_AVFormatContext) -> IO ()
+foreign import ccall safe avformat_find_stream_info
+	:: Ptr C_AVFormatContext -> Ptr (Ptr C_AVDictionary) -> IO CInt
+
+-- ffmpeg enums
+
+pattern AV_SAMPLE_FMT_U8 = 0
+pattern AV_SAMPLE_FMT_S16 = 1
+pattern AV_SAMPLE_FMT_S32 = 2
+pattern AV_SAMPLE_FMT_FLT = 3
+pattern AV_SAMPLE_FMT_DBL = 4
---
+++ flaw-ffmpeg/src/flaw-ffmpeg.c
@@ -0,0 +1,160 @@
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/imgutils.h>
+#include <libswscale/swscale.h>
+#include <signal.h>
+
+// Hacky macros to disable alarm signals temporarily.
+// FFmpeg doesn't handle syscall interruptions properly,
+// and GHC runtime uses alarms a lot.
+#define BEGIN_DISABLE_ALARMS() \
+	sigset_t disableAlarmsMask, oldMask; \
+	sigemptyset(&disableAlarmsMask); \
+	sigaddset(&disableAlarmsMask, SIGALRM); \
+	sigaddset(&disableAlarmsMask, SIGVTALRM); \
+	pthread_sigmask(SIG_BLOCK, &disableAlarmsMask, &oldMask)
+#define END_DISABLE_ALARMS() \
+	pthread_sigmask(SIG_SETMASK, &oldMask, NULL)
+
+void flaw_ffmpeg_init()
+{
+	av_register_all();
+	avformat_network_init();
+}
+
+AVFormatContext* flaw_ffmpeg_openInput(const char* url)
+{
+	AVFormatContext* ctx = NULL;
+	BEGIN_DISABLE_ALARMS();
+	int r = avformat_open_input(&ctx, url, NULL, NULL);
+	END_DISABLE_ALARMS();
+	if(r == 0) return ctx;
+	avformat_free_context(ctx);
+	return NULL;
+}
+
+static AVStream* getSingleStreamWithType(AVFormatContext* ctx, enum AVMediaType codecType)
+{
+	int r = -1;
+	for(int i = 0; i < ctx->nb_streams; ++i)
+		if(ctx->streams[i]->codec->codec_type == codecType)
+			if(r < 0) r = i;
+			else return NULL;
+	return r >= 0 ? ctx->streams[r] : NULL;
+}
+
+AVStream* flaw_ffmpeg_getSingleVideoStream(AVFormatContext* ctx)
+{
+	return getSingleStreamWithType(ctx, AVMEDIA_TYPE_VIDEO);
+}
+
+AVStream* flaw_ffmpeg_getSingleAudioStream(AVFormatContext* ctx)
+{
+	return getSingleStreamWithType(ctx, AVMEDIA_TYPE_AUDIO);
+}
+
+int flaw_ffmpeg_prepareStreamDecoding(AVStream* stream)
+{
+	int ret;
+	AVCodec* decoder = avcodec_find_decoder(stream->codec->codec_id);
+	if(!decoder) return -1;
+	if((ret = avcodec_open2(stream->codec, decoder, NULL)) < 0) return ret;
+	return 0;
+}
+
+int flaw_ffmpeg_decodeAudio(AVFormatContext* ctx, AVStream* stream, void (*output)(const void*, int),
+	int* outSamplesPerSecond, int* outFormat, int* outChannelsCount)
+{
+	// some calculations
+	int channelsCount = stream->codec->channels;
+	int bytesPerSample = av_get_bytes_per_sample(stream->codec->sample_fmt);
+	int planar = av_sample_fmt_is_planar(stream->codec->sample_fmt);
+	int bytesPerInterleavedSample = bytesPerSample * channelsCount;
+
+	// init frame
+	AVFrame* frame = av_frame_alloc();
+
+	// init packet struct
+	AVPacket pkt;
+	av_init_packet(&pkt);
+	pkt.data = NULL;
+	pkt.size = 0;
+
+	// loop reading packets
+	int err = 0, gotPacket;
+	do
+	{
+		gotPacket = av_read_frame(ctx, &pkt) >= 0;
+		if(!gotPacket)
+		{
+			pkt.data = NULL;
+			pkt.size = 0;
+		}
+		if(pkt.stream_index == stream->index)
+		{
+			AVPacket origPkt = pkt;
+			// loop decoding packets
+			int gotFrame;
+			do
+			{
+				// decode audio frame
+				gotFrame = 0;
+				int decoded = avcodec_decode_audio4(stream->codec, frame, &gotFrame, &pkt);
+				if(decoded < 0)
+				{
+					err = decoded;
+					break;
+				}
+				if(gotFrame)
+				{
+					int samplesCount = frame->nb_samples;
+					int interleavedDataSize = samplesCount * bytesPerSample * channelsCount;
+					if(planar)
+					{
+						// convert to interleaved format
+						uint8_t interleavedData[interleavedDataSize];
+						for(int i = 0; i < channelsCount; ++i)
+						{
+							uint8_t* interleavedChannelData = interleavedData + i * bytesPerSample;
+							const uint8_t* planarChannelData = frame->extended_data[i];
+							for(int j = 0; j < samplesCount; ++j)
+							{
+								uint8_t* interleavedSamplePtr = interleavedChannelData + j * bytesPerInterleavedSample;
+								const uint8_t* planarSamplePtr = planarChannelData + j * bytesPerSample;
+								for(int k = 0; k < bytesPerSample; ++k)
+									interleavedSamplePtr[k] = planarSamplePtr[k];
+							}
+						}
+						// output interleaved data
+						output(interleavedData, interleavedDataSize);
+					}
+					else
+					{
+						// output interleaved data
+						output(frame->extended_data[0], interleavedDataSize);
+					}
+				}
+				// advance packet
+				if(gotPacket)
+				{
+					pkt.data += decoded;
+					pkt.size -= decoded;
+				}
+			}
+			while(err >= 0 && (gotPacket ? pkt.size > 0 : gotFrame));
+			// free packet
+			av_packet_unref(&origPkt);
+		}
+	}
+	while(err >= 0 && gotPacket);
+
+	avcodec_close(stream->codec);
+	av_frame_free(&frame);
+
+	// output characteristics
+	*outSamplesPerSecond = stream->codec->sample_rate;
+	*outFormat = av_get_packed_sample_fmt(stream->codec->sample_fmt);
+	*outChannelsCount = channelsCount;
+
+	return err;
+}
