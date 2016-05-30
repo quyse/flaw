5ea9355fb2b0384b4ea66a31fb1dae77ff31bf18
Quyse Lert <quyse0@gmail.com>
2016-05-31 01:14:11 +0300

    remove snapshot packages from docs, fix links to point to stackage haddocks

--- sanitize_docs.sh
+++ sanitize_docs.sh
@@ -5,6 +5,14 @@
 # save copy of hscolour.css
 cp $(find $1 -name hscolour.css | head -n1) $1/
 
+# remove non-flaw packages
+pushd $1 > /dev/null
+rm -r $(ls -F | grep -P '^(?!flaw-)(?!all/).+/$')
+popd > /dev/null
+
+# get snapshot
+SNAPSHOT=$(sed -n -r -e 's/resolver:\s+([a-z0-9\-\.]+)/\1/ip' stack.yaml)
+
 for i in $(ls -F $1 | grep '/$')
 do
 	pushd $1/$i > /dev/null
@@ -24,6 +32,9 @@ do
 	sed -i -r \
 		-e 's/(<script src=")(haddock-util\.js" type="text\/javascript"><\/script>)/\1..\/\2/' \
 		-e 's/(<link href=")(ocean\.css" rel="stylesheet" type="text\/css" title="Ocean" \/>)/\1..\/\2/' \
+		-e 's/(<a href)(="\.\.\/flaw-[^"]+">)/\1_preserve\2/g' \
+		-e "s/(<a href=\")\.\.\/([^\"]+\">)/\1https:\/\/www.stackage.org\/haddock\/$SNAPSHOT\/\2/g" \
+		-e 's/(<a href)_preserve(="[^"]+">)/\1\2/g' \
 		*.html
 
 	# fix src/*.html
