ef4ebe1b301d24cc1c9f57208c3396e1bf9ea53d
Quyse Lert <quyse0@gmail.com>
2016-10-10 15:30:30 +0300

    clean flaw-canvas code up a bit

--- flaw-canvas/Flaw/Graphics/Canvas.hs
+++ flaw-canvas/Flaw/Graphics/Canvas.hs
@@ -4,7 +4,7 @@ Description: 2D painting.
 License: MIT
 -}
 
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE PatternSynonyms #-}
 
 module Flaw.Graphics.Canvas
 	( Canvas
@@ -28,13 +28,17 @@ data Canvas d = Canvas
 	{
 	-- | Blend state for usual blending.
 	  canvasBlendState :: !(BlendStateId d)
-	-- | Function for rendering bordered rectangle.
-	, canvasRenderBorderedRectangle :: forall c. Context c d => Float4 -> Float4 -> Float4 -> Float4 -> Render c ()
+	, canvasUs :: !(UniformStorage d)
+	, canvasVbBorderedRectangle :: !(VertexBufferId d)
+	, canvasUX :: !(Node Float4)
+	, canvasUY :: !(Node Float4)
+	, canvasUFillColor :: !(Node Float4)
+	, canvasUBorderColor :: !(Node Float4)
+	, canvasBorderedRectangleProgram :: !(ProgramId d)
 	}
 
 initCanvas :: Device d => d -> IO (Canvas d, IO ())
-initCanvas device = do
-	bk <- newBook
+initCanvas device = withSpecialBook $ \bk -> do
 
 	-- create blend state for usual blending
 	blendState <- book bk $ createBlendState device defaultBlendStateInfo
@@ -42,91 +46,104 @@ initCanvas device = do
 		, blendDestColor = ColorSourceInvSrcAlpha
 		}
 
-	let vbStride = sizeOf (undefined :: Float) * 10
-	let vertices = do
-		i <- [0..2] :: [Int] -- rows of quads from bottom to top
-		j <- [0..2] :: [Int] -- columns of quads from left to right
-		(x, y) <- [(1, 0), (0, 0), (0, 1), (1, 0), (0, 1), (1, 1)] -- 6 vertices per quad
-		let r =
-			[ if x + j == 0 then 1 else 0 -- x1
-			, if x + j == 1 then 1 else 0 -- x2
-			, if x + j == 2 then 1 else 0 -- x3
-			, if x + j == 3 then 1 else 0 -- x4
-			, if y + i == 0 then 1 else 0 -- y1
-			, if y + i == 1 then 1 else 0 -- y2
-			, if y + i == 2 then 1 else 0 -- y3
-			, if y + i == 3 then 1 else 0 -- y4
-			, if i == 1 && j == 1 then 1 else 0 -- use fill color?
-			, if i == 1 && j == 1 then 0 else 1 -- use border color?
-			]
-		r
-	let ic = 3 * 3 * 2 * 3
-	let ib = nullIndexBuffer
-	vb <- book bk $ withArray (vertices :: [Float]) $ \ptr -> do
-		bytes <- B.unsafePackCStringLen (castPtr ptr, vbStride * ic)
-		createStaticVertexBuffer device bytes vbStride
+	vbBorderedRectangle <- do
+		let
+			vbStride = sizeOf (undefined :: Float) * 10
+			vertices = do
+				i <- [0..2] :: [Int] -- rows of quads from bottom to top
+				j <- [0..2] :: [Int] -- columns of quads from left to right
+				(x, y) <- [(1, 0), (0, 0), (0, 1), (1, 0), (0, 1), (1, 1)] -- 6 vertices per quad
+				let r =
+					[ if x + j == 0 then 1 else 0 -- x1
+					, if x + j == 1 then 1 else 0 -- x2
+					, if x + j == 2 then 1 else 0 -- x3
+					, if x + j == 3 then 1 else 0 -- x4
+					, if y + i == 0 then 1 else 0 -- y1
+					, if y + i == 1 then 1 else 0 -- y2
+					, if y + i == 2 then 1 else 0 -- y3
+					, if y + i == 3 then 1 else 0 -- y4
+					, if i == 1 && j == 1 then 1 else 0 -- use fill color?
+					, if i == 1 && j == 1 then 0 else 1 -- use border color?
+					]
+				r
+		book bk $ withArray (vertices :: [Float]) $ \ptr -> do
+			bytes <- B.unsafePackCStringLen (castPtr ptr, vbStride * BORDERED_RECTANGLE_INDEX_COUNT)
+			createStaticVertexBuffer device bytes vbStride
 
-	-- shader for rendering bordered rectangles
 	ubs <- uniformBufferSlot 0
 	-- uniform containing four X coordinates (from left to right)
-	uX <- uniform ubs :: IO (Node Float4)
+	uX <- uniform ubs
 	-- uniform containing four Y coordinates (from bottom to top)
-	uY <- uniform ubs :: IO (Node Float4)
+	uY <- uniform ubs
 	-- uniform with fill color
 	uFillColor <- uniform ubs :: IO (Node Float4)
 	-- uniform with border color
 	uBorderColor <- uniform ubs :: IO (Node Float4)
 	us <- book bk $ createUniformStorage device ubs
-	program <- book bk $ createProgram device $ do
+	borderedRectangleProgram <- book bk $ createProgram device $ do
 		aX <- attribute 0 0 0 (AttributeVec4 AttributeFloat32)
 		aY <- attribute 0 16 0 (AttributeVec4 AttributeFloat32)
 		aC <- attribute 0 32 0 (AttributeVec2 AttributeFloat32)
 		color <- temp (uFillColor * vecFromScalar (x_ aC) + uBorderColor * vecFromScalar (y_ aC))
 		rasterize (cvec1111 (dot aX uX) (dot aY uY) (constf 0) (constf 1)) $ colorTarget 0 color
 
-	let renderBorderedRectangle xs ys fillColor borderColor = do
-		-- setup stuff
-		renderVertexBuffer 0 vb
-		renderIndexBuffer ib
-		renderUniformStorage us
-		renderProgram program
-		-- enable blending only if needed
-		renderBlendState $ if w_ fillColor >= 1 && w_ borderColor >= 1 then nullBlendState else blendState
-
-		-- set uniforms
-		renderUniform us uX xs
-		renderUniform us uY ys
-		renderUniform us uFillColor fillColor
-		renderUniform us uBorderColor borderColor
-		renderUploadUniformStorage us
-
-		-- draw
-		renderDraw ic
-
-	let canvas = Canvas
+	return Canvas
 		{ canvasBlendState = blendState
-		, canvasRenderBorderedRectangle = renderBorderedRectangle
+		, canvasUs = us
+		, canvasVbBorderedRectangle = vbBorderedRectangle
+		, canvasUX = uX
+		, canvasUY = uY
+		, canvasUFillColor = uFillColor
+		, canvasUBorderColor = uBorderColor
+		, canvasBorderedRectangleProgram = borderedRectangleProgram
 		}
-
-	return (canvas, freeBook bk)
 
 -- | Draw bordered rectangle.
 -- Coordinates are integer pixels. Y directed down.
 drawBorderedRectangle :: Context c d => Canvas d -> Int4 -> Int4 -> Float4 -> Float4 -> Render c ()
 drawBorderedRectangle Canvas
-	{ canvasRenderBorderedRectangle = renderBorderedRectangle
+	{ canvasBlendState = blendState
+	, canvasUs = us
+	, canvasVbBorderedRectangle = vbBorderedRectangle
+	, canvasUX = uX
+	, canvasUY = uY
+	, canvasUFillColor = uFillColor
+	, canvasUBorderColor = uBorderColor
+	, canvasBorderedRectangleProgram = borderedRectangleProgram
 	} (Int4 x1 x2 x3 x4) (Int4 y1 y2 y3 y4) fillColor borderColor = renderScope $ do
+
 	Vec4 viewportLeft viewportTop viewportRight viewportBottom <- renderGetViewport
-	let scaleX = 2 / fromIntegral (viewportRight - viewportLeft)
-	let scaleY = 2 / fromIntegral (viewportTop - viewportBottom)
-	let x = Float4
-		(fromIntegral x1 * scaleX - 1)
-		(fromIntegral x2 * scaleX - 1)
-		(fromIntegral x3 * scaleX - 1)
-		(fromIntegral x4 * scaleX - 1)
-	let y = Float4 -- note reverse order of y's
-		(fromIntegral y4 * scaleY + 1)
-		(fromIntegral y3 * scaleY + 1)
-		(fromIntegral y2 * scaleY + 1)
-		(fromIntegral y1 * scaleY + 1)
-	renderBorderedRectangle x y fillColor borderColor
+	let
+		scaleX = 2 / fromIntegral (viewportRight - viewportLeft)
+		scaleY = 2 / fromIntegral (viewportTop - viewportBottom)
+		xs = Float4
+			(fromIntegral x1 * scaleX - 1)
+			(fromIntegral x2 * scaleX - 1)
+			(fromIntegral x3 * scaleX - 1)
+			(fromIntegral x4 * scaleX - 1)
+		ys = Float4 -- note reverse order of y's
+			(fromIntegral y4 * scaleY + 1)
+			(fromIntegral y3 * scaleY + 1)
+			(fromIntegral y2 * scaleY + 1)
+			(fromIntegral y1 * scaleY + 1)
+
+	-- setup stuff
+	renderVertexBuffer 0 vbBorderedRectangle
+	renderIndexBuffer nullIndexBuffer
+	renderUniformStorage us
+	renderProgram borderedRectangleProgram
+	-- enable blending only if needed
+	renderBlendState $ if w_ fillColor >= 1 && w_ borderColor >= 1 then nullBlendState else blendState
+
+	-- set uniforms
+	renderUniform us uX xs
+	renderUniform us uY ys
+	renderUniform us uFillColor fillColor
+	renderUniform us uBorderColor borderColor
+	renderUploadUniformStorage us
+
+	-- draw
+	renderDraw BORDERED_RECTANGLE_INDEX_COUNT
+
+pattern BORDERED_RECTANGLE_INDEX_COUNT :: Int
+pattern BORDERED_RECTANGLE_INDEX_COUNT = 54
