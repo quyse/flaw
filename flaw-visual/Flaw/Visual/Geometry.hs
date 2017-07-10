5611fb96c040d5724eadb7bad3bd0bcfe507fd7c
Quyse Lert <quyse0@gmail.com>
2017-07-10 21:07:42 +0300

    remove degenerate triangles when indexing vertices

--- flaw-visual/Flaw/Visual/Geometry.hs
+++ flaw-visual/Flaw/Visual/Geometry.hs
@@ -122,9 +122,9 @@ indexGeometryVertices vertices = unsafePerformIO $ do
 	mVertices <- VG.thaw vertices
 	VAI.sort mVertices
 	uniqueVertices <- unique mVertices
-	indices <- VG.mapM ((fromIntegral <$>) . VAS.binarySearchL uniqueVertices) vertices
+	indices <- noDegenerateTriangles <$> VG.mapM ((fromIntegral <$>) . VAS.binarySearchL uniqueVertices) vertices
 	resultVertices <- VG.freeze uniqueVertices
-	return (resultVertices, VG.convert indices)
+	return (resultVertices, indices)
 	where
 	unique v = if VGM.null v then return v else do
 		let n = VGM.length v
@@ -140,3 +140,21 @@ indexGeometryVertices vertices = unsafePerformIO $ do
 			else return p
 		end <- f 0 0
 		return $ VGM.slice 0 (end + 1) v
+	noDegenerateTriangles v = VG.create $ do
+		let n = VG.length v
+		indices <- VGM.new n
+		let f p i =
+			if i + 2 < n then do
+				let
+					a = v VG.! i
+					b = v VG.! (i + 1)
+					c = v VG.! (i + 2)
+				if a == b || b == c || a == c then f p (i + 3)
+				else do
+					VGM.unsafeWrite indices p a
+					VGM.unsafeWrite indices (p + 1) b
+					VGM.unsafeWrite indices (p + 2) c
+					f (p + 3) (i + 3)
+			else return p
+		end <- f 0 0
+		return $ VGM.slice 0 end indices
