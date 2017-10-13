0c1ce681413a315bd9198f7928ee623847191b6e
Quyse Lert <quyse0@gmail.com>
2017-10-14 00:34:14 +0300

    rename VK to Vkontakte

--- flaw-social/Flaw/Social/Vkontakte.hs
+++ flaw-social/Flaw/Social/Vkontakte.hs
@@ -1,20 +1,20 @@
 {-|
-Module: Flaw.Social.VK
-Description: VK.com integration.
+Module: Flaw.Social.Vkontakte
+Description: vk.com integration.
 License: MIT
 -}
 
 {-# LANGUAGE CPP, GeneralizedNewtypeDeriving, JavaScriptFFI, OverloadedStrings, TypeFamilies #-}
 
-module Flaw.Social.VK
-	( VK()
+module Flaw.Social.Vkontakte
+	( Vkontakte()
 	, SocialUserId(..)
 	, SocialUserToken(..)
 #if defined(ghcjs_HOST_OS)
-	, initVKiframe
+	, initVkontakteIframe
 #else
-	, initVK
-	, vkHeadScripts
+	, initVkontakte
+	, vkontakteHeadScripts
 #endif
 	) where
 
@@ -47,29 +47,29 @@ import qualified Text.Blaze.Html5.Attributes as A
 
 #endif
 
-instance Social VK where
-	newtype SocialUserId VK = VKUserId B.ByteString deriving S.Serialize
-	newtype SocialUserToken VK = VKUserToken B.ByteString deriving S.Serialize
-	socialUniversalUserId (VKUserId userId) = "vk" <> userId
+instance Social Vkontakte where
+	newtype SocialUserId Vkontakte = VkontakteUserId B.ByteString deriving S.Serialize
+	newtype SocialUserToken Vkontakte = VkontakteUserToken B.ByteString deriving S.Serialize
+	socialUniversalUserId (VkontakteUserId userId) = "vkontakte" <> userId
 
 #if defined(ghcjs_HOST_OS)
 
-data VK = VK
+data Vkontakte = Vkontakte
 
--- | Init IFrame VK application.
-initVKiframe :: IO VK
-initVKiframe = do
+-- | Init IFrame Vkontakte application.
+initVkontakteIframe :: IO Vkontakte
+initVkontakteIframe = do
 	initialized <- js_init_iframe
-	unless initialized $ throwIO $ DescribeFirstException ("failed to initialize VK iframe client" :: T.Text)
-	return VK
+	unless initialized $ throwIO $ DescribeFirstException ("failed to initialize Vkontakte iframe client" :: T.Text)
+	return Vkontakte
 
-instance SocialClient VK where
-	authSocialClient VK = do
+instance SocialClient Vkontakte where
+	authSocialClient Vkontakte = do
 		viewerId <- T.encodeUtf8 . textFromJSString <$> js_viewerId
 		if B.null viewerId then return Nothing
 		else do
 			authKey <- T.encodeUtf8 . textFromJSString <$> js_authKey
-			return $ Just (VKUserId viewerId, VKUserToken authKey)
+			return $ Just (VkontakteUserId viewerId, VkontakteUserToken authKey)
 
 foreign import javascript interruptible "h$flaw_social_vk_init_iframe($c);" js_init_iframe :: IO Bool
 foreign import javascript unsafe "$r = h$flaw_social_vk_viewer_id" js_viewerId :: IO JSString
@@ -77,33 +77,33 @@ foreign import javascript unsafe "$r = h$flaw_social_vk_auth_key" js_authKey :: 
 
 #else
 
-data VK = VK
-	{ vkAppIdBytes :: !B.ByteString
-	, vkAppSecretBytes :: !B.ByteString
+data Vkontakte = Vkontakte
+	{ vkontakteAppIdBytes :: !B.ByteString
+	, vkontakteAppSecretBytes :: !B.ByteString
 	}
 
-initVK :: T.Text -> T.Text -> IO VK
-initVK appId appSecret = return VK
-	{ vkAppIdBytes = T.encodeUtf8 appId
-	, vkAppSecretBytes = T.encodeUtf8 appSecret
+initVkontakte :: T.Text -> T.Text -> IO Vkontakte
+initVkontakte appId appSecret = return Vkontakte
+	{ vkontakteAppIdBytes = T.encodeUtf8 appId
+	, vkontakteAppSecretBytes = T.encodeUtf8 appSecret
 	}
 
-instance SocialServer VK where
-	authSocialClientByRequest vk getParam = do
+instance SocialServer Vkontakte where
+	authSocialClientByRequest vkontakte getParam = do
 		maybeViewerId <- getParam "viewer_id"
 		maybeAuthKey <- getParam "auth_key"
 		case (maybeViewerId, maybeAuthKey) of
 			(Just viewerId, Just authKey) -> do
-				let userId = VKUserId $ T.encodeUtf8 viewerId
-				ok <- liftIO $ verifySocialUserToken vk userId $ VKUserToken $ T.encodeUtf8 authKey
+				let userId = VkontakteUserId $ T.encodeUtf8 viewerId
+				ok <- liftIO $ verifySocialUserToken vkontakte userId $ VkontakteUserToken $ T.encodeUtf8 authKey
 				return $ if ok then Just userId else Nothing
 			_ -> return Nothing
 	-- | Verify user token.
 	-- TODO: check via secure.checkToken.
-	verifySocialUserToken VK
-		{ vkAppIdBytes = appIdBytes
-		, vkAppSecretBytes = appSecretBytes
-		} (VKUserId userId) (VKUserToken userToken) = return $ userToken == userTokenShouldBe
+	verifySocialUserToken Vkontakte
+		{ vkontakteAppIdBytes = appIdBytes
+		, vkontakteAppSecretBytes = appSecretBytes
+		} (VkontakteUserId userId) (VkontakteUserToken userToken) = return $ userToken == userTokenShouldBe
 		where userTokenShouldBe = BL.toStrict $ B.toLazyByteString $ B.byteStringHex $ BA.convert
 			$ (hash :: B.ByteString -> Digest MD5) $ BL.toStrict $ B.toLazyByteString
 			$  B.byteString appIdBytes
@@ -112,7 +112,7 @@ instance SocialServer VK where
 			<> B.char7 '_'
 			<> B.byteString appSecretBytes
 
-vkHeadScripts :: H.Html
-vkHeadScripts = H.script H.! A.src "//vk.com/js/api/xd_connection.js?2" $ mempty
+vkontakteHeadScripts :: H.Html
+vkontakteHeadScripts = H.script H.! A.src "//vk.com/js/api/xd_connection.js?2" $ mempty
 
 #endif
