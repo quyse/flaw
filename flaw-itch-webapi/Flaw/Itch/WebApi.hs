9e9842d427c55685641fc5b5acbbad8c8c8949f9
Quyse Lert <quyse0@gmail.com>
2018-01-14 19:15:49 +0300

    simplify itch web api, allow use of multiple api keys

--- flaw-itch-webapi/Flaw/Itch/WebApi.hs
+++ flaw-itch-webapi/Flaw/Itch/WebApi.hs
@@ -7,8 +7,7 @@ License: MIT
 {-# LANGUAGE DeriveGeneric, GeneralizedNewtypeDeriving, OverloadedStrings, ViewPatterns #-}
 
 module Flaw.Itch.WebApi
-	( ItchWebApi()
-	, newItchWebApi
+	( ItchWebApiKey(..)
 	, itchWebApiMe
 	, itchWebApiDownloadKeys
 	, ItchUserId(..)
@@ -18,10 +17,11 @@ module Flaw.Itch.WebApi
 	, ItchDownloadKey(..)
 	) where
 
+import Control.Monad
 import qualified Data.Aeson as A
+import qualified Data.Aeson.Types as A
 import qualified Data.ByteString as B
 import Data.Hashable
-import qualified Data.HashMap.Strict as HM
 import Data.Monoid
 import Data.String
 import qualified Data.Text as T
@@ -30,52 +30,34 @@ import Data.Word
 import GHC.Generics(Generic)
 import qualified Network.HTTP.Client as H
 
--- | Itch Web Api.
-data ItchWebApi = ItchWebApi
-	{ itchWebApiHttpManager :: !H.Manager
-	, itchWebApiHttpRequest :: !H.Request
-	}
+-- | Itch API key (normal API key or JWT token).
+data ItchWebApiKey = ItchWebApiKey !T.Text {- | Is API key -} !Bool
 
-newItchWebApi :: H.Manager -> T.Text -> IO ItchWebApi
-newItchWebApi httpManager apiKey = return ItchWebApi
-	{ itchWebApiHttpManager = httpManager
-	, itchWebApiHttpRequest = H.defaultRequest
+itchWebApiRequest :: H.Manager -> ItchWebApiKey -> B.ByteString -> [(B.ByteString, Maybe B.ByteString)] -> IO A.Value
+itchWebApiRequest httpManager (ItchWebApiKey apiKey isKey) path params =
+	either fail return . A.eitherDecode' . H.responseBody =<< H.httpLbs (H.setQueryString params H.defaultRequest
 		{ H.method = "GET"
 		, H.secure = True
 		, H.host = "itch.io"
 		, H.port = 443
-		, H.path = "/api/1/" <> T.encodeUtf8 apiKey
-		}
-	}
-
-itchWebApiRequest :: ItchWebApi -> B.ByteString -> [(B.ByteString, Maybe B.ByteString)] -> IO A.Value
-itchWebApiRequest ItchWebApi
-	{ itchWebApiHttpManager = httpManager
-	, itchWebApiHttpRequest = httpRequest
-	} path params = do
-	Just value <- A.decode . H.responseBody <$> H.httpLbs (H.setQueryString params httpRequest
-		{ H.path = H.path httpRequest <> path
+		, H.path = "/api/1/" <> (if isKey then "key" else "jwt") <> path
+		, H.requestHeaders = [("Authorization", "Bearer " <> T.encodeUtf8 apiKey)]
 		}) httpManager
-	return value
 
-itchWebApiMe :: ItchWebApi -> T.Text -> Bool -> IO (Maybe ItchUser)
-itchWebApiMe ItchWebApi
-	{ itchWebApiHttpManager = httpManager
-	, itchWebApiHttpRequest = httpRequest
-	} token isKey =
-	(itchMeResponse_user <$>) . A.decode' . H.responseBody <$> H.httpLbs httpRequest
-		{ H.path = "/api/1/" <> (if isKey then "key" else "jwt") <> "/me"
-		, H.requestHeaders = ("Authorization", T.encodeUtf8 token) : H.requestHeaders httpRequest
-		} httpManager
+itchWebApiMe :: H.Manager -> ItchWebApiKey -> IO (Maybe (ItchUser, ItchGameId))
+itchWebApiMe httpManager apiKey =
+	A.parseMaybe parseResponse <$> itchWebApiRequest httpManager apiKey "/me" []
+	where parseResponse = A.withObject "response" $ \response -> do
+		user <- response A..: "user"
+		gameId <- (A..: "game_id") =<< A.withObject "issuer" return =<< (A..: "issuer") =<< A.withObject "api_key" return =<< response A..: "api_key"
+		return (user, gameId)
 
-itchWebApiDownloadKeys :: ItchWebApi -> ItchGameId -> ItchUserId -> IO (Maybe ItchDownloadKey)
-itchWebApiDownloadKeys api (ItchGameId gameId) (ItchUserId userId) = do
-	value <- itchWebApiRequest api ("game/" <> fromString (show gameId) <> "/download_keys")
+itchWebApiDownloadKeys :: H.Manager -> ItchWebApiKey -> ItchGameId -> ItchUserId -> IO (Maybe ItchDownloadKey)
+itchWebApiDownloadKeys httpManager apiKey (ItchGameId gameId) (ItchUserId userId) = do
+	A.parseMaybe parseResponse <$> itchWebApiRequest httpManager apiKey ("/game/" <> fromString (show gameId) <> "/download_keys")
 		[ ("user_id", Just $ fromString $ show userId)
 		]
-	return $ case value of
-		A.Object (HM.lookup "download_key" -> Just (A.fromJSON -> A.Success downloadKey)) -> Just downloadKey
-		_ -> Nothing
+	where parseResponse = A.withObject "response" (A..: "download_key")
 
 newtype ItchUserId = ItchUserId Word64 deriving (Eq, Ord, Hashable, Generic, Show, A.FromJSON, A.ToJSON)
 data ItchUser = ItchUser
@@ -91,10 +73,28 @@ instance A.FromJSON ItchUser where
 
 data ItchMeResponse = ItchMeResponse
 	{ itchMeResponse_user :: !ItchUser
+	, itchMeResponse_api_key :: !ItchMeApiKey
 	} deriving (Generic, Show)
 instance A.FromJSON ItchMeResponse where
 	parseJSON = A.genericParseJSON A.defaultOptions
 		{ A.fieldLabelModifier = drop 15
+		}
+
+data ItchMeApiKey = ItchMeApiKey
+	{ itchMeApiKey_type :: !T.Text
+	, itchMeApiKey_issuer :: !ItchMeApiKeyIssuer
+	} deriving (Generic, Show)
+instance A.FromJSON ItchMeApiKey where
+	parseJSON = A.genericParseJSON A.defaultOptions
+		{ A.fieldLabelModifier = drop 13
+		}
+
+data ItchMeApiKeyIssuer = ItchMeApiKeyIssuer
+	{ itchMeApiKeyIssuer_game_id :: {-# UNPACK #-} !ItchGameId
+	} deriving (Generic, Show)
+instance A.FromJSON ItchMeApiKeyIssuer where
+	parseJSON = A.genericParseJSON A.defaultOptions
+		{ A.fieldLabelModifier = drop 19
 		}
 
 newtype ItchGameId = ItchGameId Word64 deriving (Eq, Ord, Hashable, Generic, Show, A.FromJSON, A.ToJSON)
@@ -102,7 +102,7 @@ newtype ItchGameId = ItchGameId Word64 deriving (Eq, Ord, Hashable, Generic, Sho
 newtype ItchDownloadKeyId = ItchDownloadKeyId Word64 deriving (Eq, Ord, Hashable, Generic, Show, A.FromJSON, A.ToJSON)
 data ItchDownloadKey = ItchDownloadKey
 	{ itchDownloadKey_id :: !ItchDownloadKeyId
-	, itchDownloadKey_game_id :: !ItchGameId
+	, itchDownloadKey_game_id :: {-# UNPACK #-} !ItchGameId
 	, itchDownloadKey_owner :: !ItchUser
 	} deriving (Generic, Show)
 instance A.FromJSON ItchDownloadKey where
