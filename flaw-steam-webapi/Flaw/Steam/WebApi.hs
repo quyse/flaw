59377c4093480459bf51e85ff595567b7766071d
Quyse Lert <quyse0@gmail.com>
2018-04-05 04:12:22 +0300

    steam webapi player summary method

--- flaw-steam-webapi/Flaw/Steam/WebApi.hs
+++ flaw-steam-webapi/Flaw/Steam/WebApi.hs
@@ -9,12 +9,15 @@ License: MIT
 module Flaw.Steam.WebApi
 	( SteamWebApi()
 	, newSteamWebApi
+	, steamWebApi_ISteamUser_GetPlayerSummary
+	, SteamPlayerSummary(..)
 	, steamWebApiAuthenticateUserTicket
 	, SteamTicket(..)
 	, steamWebApiCheckAppOwnership
 	) where
 
-import qualified Data.Aeson as A
+import qualified Data.Aeson as J
+import qualified Data.Aeson.Types as J
 import qualified Data.ByteArray.Encoding as BA
 import qualified Data.ByteString as B
 import qualified Data.HashMap.Strict as HM
@@ -22,6 +25,7 @@ import Data.Monoid
 import Data.String
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
+import qualified Data.Vector as V
 import GHC.Generics(Generic)
 import qualified Network.HTTP.Client as H
 
@@ -35,8 +39,8 @@ data SteamWebApi = SteamWebApi
 	, steamWebApiKey :: !B.ByteString
 	}
 
-newSteamWebApi :: H.Manager -> Int -> T.Text -> IO SteamWebApi
-newSteamWebApi httpManager appId apiKey = return SteamWebApi
+newSteamWebApi :: H.Manager -> Int -> T.Text -> SteamWebApi
+newSteamWebApi httpManager appId apiKey = SteamWebApi
 	{ steamWebApiHttpManager = httpManager
 	, steamWebApiHttpRequest = H.defaultRequest
 		{ H.method = "GET"
@@ -49,22 +53,39 @@ newSteamWebApi httpManager appId apiKey = return SteamWebApi
 	, steamWebApiKey = T.encodeUtf8 apiKey
 	}
 
-steamWebApiRequest :: SteamWebApi -> B.ByteString -> [(B.ByteString, Maybe B.ByteString)] -> IO A.Value
+steamWebApiRequest :: J.FromJSON a => SteamWebApi -> B.ByteString -> [(B.ByteString, Maybe B.ByteString)] -> IO a
 steamWebApiRequest SteamWebApi
 	{ steamWebApiHttpManager = httpManager
 	, steamWebApiHttpRequest = httpRequest
 	, steamWebApiAppId = appId
 	, steamWebApiKey = apiKey
 	} path params = do
-	Just value <- A.decode . H.responseBody <$> H.httpLbs (H.setQueryString (("appid", Just appId) : ("key", Just apiKey) : params) httpRequest
+	Just value <- J.decode . H.responseBody <$> H.httpLbs (H.setQueryString (("appid", Just appId) : ("key", Just apiKey) : params) httpRequest
 		{ H.path = H.path httpRequest <> path
 		}) httpManager
 	return value
 
+steamWebApi_ISteamUser_GetPlayerSummary :: SteamWebApi -> SteamId -> IO (Maybe SteamPlayerSummary)
+steamWebApi_ISteamUser_GetPlayerSummary api (SteamId steamId) =
+	parseResponse =<< steamWebApiRequest api "ISteamUser/GetPlayerSummaries/v2" [("steamids", Just $ fromString $ show steamId)]
+	where parseResponse = (either fail return .) . J.parseEither $ \o -> do
+		playerSummaries <- (J..: "players") =<< (o J..: "response")
+		return $ if V.null playerSummaries then Nothing else Just (playerSummaries V.! 0)
+
+data SteamPlayerSummary = SteamPlayerSummary
+	{ steamPlayerSummary_personaname :: !T.Text
+	, steamPlayerSummary_profileurl :: !T.Text
+	, steamPlayerSummary_avatar :: !T.Text
+	} deriving Generic
+instance J.FromJSON SteamPlayerSummary where
+	parseJSON = J.genericParseJSON J.defaultOptions
+		{ J.fieldLabelModifier = drop 19
+		}
+
 steamWebApiAuthenticateUserTicket :: SteamWebApi -> B.ByteString -> IO SteamTicket
 steamWebApiAuthenticateUserTicket api ticket = do
-	A.Object (HM.lookup "response" -> Just (A.Object (HM.lookup "params" -> Just
-		(A.fromJSON -> A.Success AuthenticatedUserTicket
+	J.Object (HM.lookup "response" -> Just (J.Object (HM.lookup "params" -> Just
+		(J.fromJSON -> J.Success AuthenticatedUserTicket
 			{ aut_result = "OK"
 			, aut_steamid = SteamId . read . T.unpack -> steamId
 			, aut_ownersteamid = SteamId . read . T.unpack -> ownerSteamId
@@ -94,14 +115,14 @@ data AuthenticatedUserTicket = AuthenticatedUserTicket
 	, aut_vacbanned :: !Bool
 	, aut_publisherbanned :: !Bool
 	} deriving Generic
-instance A.FromJSON AuthenticatedUserTicket where
-	parseJSON = A.genericParseJSON A.defaultOptions
-		{ A.fieldLabelModifier = drop 4
+instance J.FromJSON AuthenticatedUserTicket where
+	parseJSON = J.genericParseJSON J.defaultOptions
+		{ J.fieldLabelModifier = drop 4
 		}
 
 steamWebApiCheckAppOwnership :: SteamWebApi -> SteamId -> IO Bool
 steamWebApiCheckAppOwnership api (SteamId steamId) = do
-	A.Object (HM.lookup "appownership" -> Just (A.fromJSON -> A.Success AppOwnership
+	J.Object (HM.lookup "appownership" -> Just (J.fromJSON -> J.Success AppOwnership
 		{ ao_result = "OK"
 		, ao_ownsapp = ownsApp
 		})) <- steamWebApiRequest api "ISteamUser/CheckAppOwnership/V0001/"
@@ -116,7 +137,7 @@ data AppOwnership = AppOwnership
 	-- , ao_timestamp :: !T.Text
 	-- , ao_ownersteamid :: !T.Text
 	} deriving Generic
-instance A.FromJSON AppOwnership where
-	parseJSON = A.genericParseJSON A.defaultOptions
-		{ A.fieldLabelModifier = drop 3
+instance J.FromJSON AppOwnership where
+	parseJSON = J.genericParseJSON J.defaultOptions
+		{ J.fieldLabelModifier = drop 3
 		}
